b3a160aa4a779d0d1b729ff5cfe4059d10c6c172
Arsalan Savand <ArsalanSavand@users.noreply.github.com>
2019-05-17 22:21:13 +0300

    Add blog, user and feed components to main route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,24 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [{
+  path: 'blog',
+  loadChildren: './components/blog/blog.module#BlogModule'
+}, {
+  path: 'feed',
+  loadChildren: './components/feed/feed.module#FeedModule'
+}, {
+  path: 'user',
+  loadChildren: './components/user/user.module#UserModule'
+}, {
+  path: '**',
+  redirectTo: 'feed',
+  pathMatch: 'full'
+}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
