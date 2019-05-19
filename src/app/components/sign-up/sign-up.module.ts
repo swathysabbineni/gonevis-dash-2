96cb948213da2dca1763ee6a9a41e048a407a101
Arsalan Savand <ArsalanSavand@users.noreply.github.com>
2019-05-20 00:27:40 +0300

    Add translation to SignUpComponent

--- src/app/components/sign-up/sign-up.module.ts
+++ src/app/components/sign-up/sign-up.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { TranslateModule } from '@ngx-translate/core';
 
 import { SignUpRoutingModule } from './sign-up-routing.module';
 import { SignUpComponent } from './sign-up.component';
@@ -11,6 +12,7 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
     CommonModule,
     FormsModule,
     ReactiveFormsModule,
+    TranslateModule.forChild(),
     SignUpRoutingModule,
   ],
 })
