c4786e0306240cd539dd748b680f597f8d8d35ac
Arsalan Savand <ArsalanSavand@users.noreply.github.com>
2019-05-19 22:59:08 +0300

    Add HttpErrorResponseApi

---
+++ src/app/models/http-error-response-api/http-error-response-api.spec.ts
@@ -0,0 +1,7 @@
+import { HttpErrorResponseApi } from './http-error-response-api';
+
+describe('HttpErrorResponseApi', () => {
+  it('should create an instance', () => {
+    expect(new HttpErrorResponseApi()).toBeTruthy();
+  });
+});
