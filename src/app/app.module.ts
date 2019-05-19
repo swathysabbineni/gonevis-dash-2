a6b032f7824f9cc753b0b525a58bbea13f70c4a1
Arsalan Savand <ArsalanSavand@users.noreply.github.com>
2019-05-20 00:26:53 +0300

    Setup and import TranslateModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,23 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { JwtInterceptorService } from '@app/services/jwt-interceptor/jwt-interceptor.service';
+import { TranslateLoader, TranslateModule } from '@ngx-translate/core';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CookieService } from 'ngx-cookie-service';
-import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
+import { HTTP_INTERCEPTORS, HttpClient, HttpClientModule } from '@angular/common/http';
+
+/**
+ * Loads translations from given prefix.
+ *
+ * @param http Performs HTTP requests.
+ *
+ * @returns An instance of the loader.
+ */
+export function createTranslateLoader(http: HttpClient): TranslateHttpLoader {
+  return new TranslateHttpLoader(http, '../assets/i18n/', '.json');
+}
 
 @NgModule({
   declarations: [
@@ -14,6 +27,13 @@ import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
+    TranslateModule.forRoot({
+      loader: {
+        provide: TranslateLoader,
+        useFactory: (createTranslateLoader),
+        deps: [HttpClient],
+      },
+    }),
   ],
   providers: [
     CookieService,
