e4fded1bd4e6341fb5c3e799c4432afce5609872
Arsalan Savand <ArsalanSavand@users.noreply.github.com>
2019-05-21 09:43:47 +0300

    Add a method to get current route's data and set window title based on given title

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,9 @@
 import { Component } from '@angular/core';
+import { Title } from '@angular/platform-browser';
+import { ActivatedRoute, Data, NavigationEnd, Router, RouterEvent } from '@angular/router';
 import { TranslateService } from '@ngx-translate/core';
+import { Observable } from 'rxjs';
+import { filter, map, mergeMap } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
@@ -9,7 +13,23 @@ import { TranslateService } from '@ngx-translate/core';
 export class AppComponent {
   title = 'gonevis';
 
-  constructor(private translate: TranslateService) {
+  constructor(private router: Router, private activatedRoute: ActivatedRoute,
+              private titleService: Title,
+              private translate: TranslateService) {
     this.translate.setDefaultLang('en');
+    // Set window title
+    this.router.events.pipe(
+      filter((event: RouterEvent): boolean => event instanceof NavigationEnd),
+      map((): ActivatedRoute => this.activatedRoute),
+      map((route: ActivatedRoute): ActivatedRoute => {
+        while (route.firstChild) {
+          route = route.firstChild;
+        }
+        return route;
+      }),
+      filter((route: ActivatedRoute): boolean => route.outlet === 'primary'),
+      mergeMap((route: ActivatedRoute): Observable<Data> => route.data),
+    )
+      .subscribe((event: Data): void => this.titleService.setTitle(event.title));
   }
 }
