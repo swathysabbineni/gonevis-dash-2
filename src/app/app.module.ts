69b67d0a755f77953ca9cc16575fbaea3bd25afc
Arsalan Savand <ArsalanSavand@users.noreply.github.com>
2019-05-21 10:20:36 +0300

    Rename JwtInterceptorService to AuthInterceptorService  at AppModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { JwtInterceptorService } from '@app/services/jwt-interceptor/jwt-interceptor.service';
+import { AuthInterceptorService } from '@app/services/auth-interceptor/auth-interceptor.service';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CookieService } from 'ngx-cookie-service';
@@ -17,7 +17,7 @@ import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
   ],
   providers: [
     CookieService,
-    { provide: HTTP_INTERCEPTORS, useClass: JwtInterceptorService, multi: true },
+    { provide: HTTP_INTERCEPTORS, useClass: AuthInterceptorService, multi: true },
   ],
   bootstrap: [AppComponent],
 })
