42b5e7cbb061f55e48bb7a6229fd0266a839bd7a
Arsalan Savand <ArsalanSavand@users.noreply.github.com>
2019-05-22 10:49:17 +0300

    Update AppComponent to get user data

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { UserAuth } from '@app/interfaces/user-auth';
+import { AuthService } from '@app/services/auth/auth.service';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,14 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'gonevis';
+  isCollapsed: boolean;
+  user: UserAuth;
+
+  constructor(private authService: AuthService) {
+    // Subscribe to AuthService's user changes.
+    this.authService.user.subscribe((user: UserAuth): void => {
+      this.user = user;
+    });
+    this.isCollapsed = true;
+  }
 }
