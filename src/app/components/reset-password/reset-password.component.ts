3188aad4cb24847e011d50b36c3a4304707cb691
Arsalan Savand <ArsalanSavand@users.noreply.github.com>
2019-05-24 14:08:32 +0300

    Add ResetPasswordComponent

---
+++ src/app/components/reset-password/reset-password.component.ts
@@ -0,0 +1,85 @@
+import { Component, OnInit } from '@angular/core';
+import { AbstractControl, FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ActivatedRoute, Params, Router } from '@angular/router';
+import { ApiError } from '@app/interfaces/api-error';
+import { HttpErrorResponseApi } from '@app/models/http-error-response-api';
+import { AuthService } from '@app/services/auth/auth.service';
+import { UserService } from '@app/services/user/user.service';
+import { TranslateService } from '@ngx-translate/core';
+
+@Component({
+  selector: 'app-reset-password',
+  templateUrl: './reset-password.component.html',
+  styleUrls: ['./reset-password.component.scss'],
+})
+export class ResetPasswordComponent implements OnInit {
+
+  // Reset password token
+  token: string;
+
+  // Reset password form
+  form: FormGroup;
+
+  // API errors
+  error: ApiError = {};
+
+  // API loading indicator
+  loading: boolean;
+
+  constructor(private formBuilder: FormBuilder,
+              private router: Router,
+              private activatedRoute: ActivatedRoute,
+              private translateService: TranslateService,
+              private authService: AuthService,
+              private userService: UserService) {
+  }
+
+  ngOnInit() {
+    // Subscribe to active route's query params.
+    this.activatedRoute.queryParams.subscribe((params: Params): void => {
+      // Get and set verification token from url query param.
+      this.token = params.token;
+    });
+    // Setup the form
+    this.form = this.formBuilder.group({
+      password: ['', Validators.required],
+      password2: ['', Validators.required],
+    });
+  }
+
+  /**
+   * @return Reset password form controls (fields)
+   */
+  get f(): { [p: string]: AbstractControl } {
+    return this.form.controls;
+  }
+
+  /**
+   * Reset password
+   */
+  submit(): void {
+    // Validate form
+    if (this.form.invalid) {
+      return;
+    }
+    // If password 1 and password 2 were not equal, then set error.
+    if (this.f.password.value !== this.f.password2.value) {
+      this.translateService.get('ERROR_PASSWORD_MISMATCH').subscribe((response: string): void => {
+        this.error.non_field_errors = [response];
+      });
+      return;
+    }
+    // Save token to reset password
+    this.authService.setToken(this.token);
+    this.loading = true;
+    // API call
+    this.userService.resetPassword(this.f.password.value).subscribe((): void => {
+      this.authService.signOut();
+      this.router.navigateByUrl('/sign-in');
+      this.loading = false;
+    }, (error: HttpErrorResponseApi): void => {
+      this.error = error.error;
+      this.loading = false;
+    });
+  }
+}
