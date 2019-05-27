f106965f64675119fc53e28ad8369bfde3fe47a8
Arsalan Savand <ArsalanSavand@users.noreply.github.com>
2019-05-27 15:21:48 +0300

    Add two new methods to FeedService

--- src/app/components/feed/feed.service.ts
+++ src/app/components/feed/feed.service.ts
@@ -1,9 +1,34 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { ApiResponse } from '@app/interfaces/api-response';
+import { ApiResponseCreated } from '@app/interfaces/api-response-created';
+import { EntryFeed } from '@app/interfaces/entry-feed';
+import { ApiService } from '@app/services/api/api.service';
+import { Observable } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class FeedService {
 
-  constructor() { }
+  constructor(private http: HttpClient, private apiService: ApiService) {
+  }
+
+  /**
+   * Gets list of entries based on given endpoint.
+   *
+   * @param endpoint Endpoint
+   */
+  getEntries(endpoint: string): Observable<ApiResponse<EntryFeed>> {
+    return this.http.get<ApiResponse<EntryFeed>>(`${this.apiService.base.v1}${endpoint}`);
+  }
+
+  /**
+   * Toggle entry like by user
+   *
+   * @param id Entry ID
+   */
+  likeEntry(id: string): Observable<ApiResponseCreated> {
+    return this.http.post<ApiResponseCreated>(`${this.apiService.base.zero}website/entry/${id}/vote/`, null);
+  }
 }
