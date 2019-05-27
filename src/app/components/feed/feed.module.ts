13857d48e722ae89bbbcc46872984a3116bdc9da
Arsalan Savand <ArsalanSavand@users.noreply.github.com>
2019-05-27 15:22:16 +0300

    Import heart icon from solid icons

--- src/app/components/feed/feed.module.ts
+++ src/app/components/feed/feed.module.ts
@@ -3,6 +3,7 @@ import { NgModule } from '@angular/core';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { library } from '@fortawesome/fontawesome-svg-core';
 import { faBookmark, faComment, faHeart, faShareSquare } from '@fortawesome/free-regular-svg-icons';
+import { faHeart as heartFill } from '@fortawesome/free-solid-svg-icons';
 import { faStar } from '@fortawesome/free-regular-svg-icons/faStar';
 import { faHashtag } from '@fortawesome/free-solid-svg-icons';
 import { TranslateModule } from '@ngx-translate/core';
@@ -24,6 +25,7 @@ import { FeedComponent } from './feed.component';
 export class FeedModule {
   constructor() {
     library.add(faHeart);
+    library.add(heartFill);
     library.add(faComment);
     library.add(faBookmark);
     library.add(faShareSquare);
