a4fd150e33dcc0eb95f81337b589efeb7c8a79ce
Arsalan Savand <ArsalanSavand@users.noreply.github.com>
2019-05-31 10:50:18 +0300

    Fix icons raising error

--- src/app/components/feed/feed.module.ts
+++ src/app/components/feed/feed.module.ts
@@ -3,9 +3,7 @@ import { NgModule } from '@angular/core';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { library } from '@fortawesome/fontawesome-svg-core';
 import { faBookmark, faComment, faHeart, faShareSquare } from '@fortawesome/free-regular-svg-icons';
-import { faHeart as faHeartFill } from '@fortawesome/free-solid-svg-icons';
-import { faStar } from '@fortawesome/free-regular-svg-icons/faStar';
-import { faHashtag } from '@fortawesome/free-solid-svg-icons';
+import { faHeart as faHeartFill, faStar, faHashtag } from '@fortawesome/free-solid-svg-icons';
 import { TranslateModule } from '@ngx-translate/core';
 import { TooltipModule } from 'ngx-bootstrap';
 
