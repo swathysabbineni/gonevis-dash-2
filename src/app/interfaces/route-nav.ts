0de3e1ac1e0b2c300da9e0f526ec7ada0f02da46
Arsalan Savand <ArsalanSavand@users.noreply.github.com>
2019-05-31 10:49:57 +0300

    Update RouteNav interface

--- src/app/interfaces/route-nav.ts
+++ src/app/interfaces/route-nav.ts
@@ -1,4 +1,7 @@
+import { ApiResponse } from '@app/interfaces/api-response';
+import { EntryFeed } from '@app/interfaces/entry-feed';
 import { IconProp } from '@fortawesome/fontawesome-svg-core';
+import { Observable } from 'rxjs';
 
 /**
  * Represents a navigation item with routing icons
@@ -6,6 +9,6 @@ import { IconProp } from '@fortawesome/fontawesome-svg-core';
 export interface RouteNav {
   label: string;
   route: string;
-  endpoint: string;
+  api: Observable<ApiResponse<EntryFeed>>;
   icon: IconProp;
 }
