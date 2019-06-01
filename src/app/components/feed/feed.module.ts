6737ebc9e4f64c0ecada3798e7fdbab304ceb25b
Arsalan Savand <ArsalanSavand@users.noreply.github.com>
2019-06-01 16:43:40 +0300

    Remove unwanted imports and update FeedModule

--- src/app/components/feed/feed.module.ts
+++ src/app/components/feed/feed.module.ts
@@ -1,20 +1,11 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
-import { EntryShareModule } from '@app/shared/entry-share/entry-share.module';
+import { EntryListModule } from '@app/shared/entry-list/entry-list.module';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { library } from '@fortawesome/fontawesome-svg-core';
 import { faBookmark } from '@fortawesome/free-regular-svg-icons/faBookmark';
-import { faComment } from '@fortawesome/free-regular-svg-icons/faComment';
-import { faEye } from '@fortawesome/free-regular-svg-icons/faEye';
-import { faHeart } from '@fortawesome/free-regular-svg-icons/faHeart';
-import { faShareSquare } from '@fortawesome/free-regular-svg-icons/faShareSquare';
-import { faHeart as faHeartFill } from '@fortawesome/free-solid-svg-icons';
-import { faBookmark as faBookmarkFill } from '@fortawesome/free-solid-svg-icons/faBookmark';
 import { faHashtag } from '@fortawesome/free-solid-svg-icons/faHashtag';
-import { faStar } from '@fortawesome/free-solid-svg-icons/faStar';
 import { TranslateModule } from '@ngx-translate/core';
-import { PopoverModule, TooltipModule } from 'ngx-bootstrap';
-import { InfiniteScrollModule } from 'ngx-infinite-scroll';
 
 import { FeedRoutingModule } from './feed-routing.module';
 import { FeedComponent } from './feed.component';
@@ -26,23 +17,12 @@ import { FeedComponent } from './feed.component';
     FeedRoutingModule,
     FontAwesomeModule,
     TranslateModule.forChild(),
-    TooltipModule.forRoot(),
-    InfiniteScrollModule,
-    PopoverModule.forRoot(),
-    InfiniteScrollModule,
-    EntryShareModule,
+    EntryListModule,
   ],
 })
 export class FeedModule {
   constructor() {
-    library.add(faHeart);
-    library.add(faHeartFill);
-    library.add(faComment);
-    library.add(faEye);
     library.add(faBookmark);
-    library.add(faBookmarkFill);
-    library.add(faShareSquare);
-    library.add(faStar);
     library.add(faHashtag);
   }
 }
