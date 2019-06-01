2a4780ace39b37848530c26dd6a0614f11320cc2
Arsalan Savand <ArsalanSavand@users.noreply.github.com>
2019-06-01 20:27:29 +0300

    Remove api property from FeedNav interface

--- src/app/interfaces/feed-nav.ts
+++ src/app/interfaces/feed-nav.ts
@@ -1,14 +1,9 @@
-import { ApiResponse } from '@app/interfaces/api-response';
-import { EntryFeed } from '@app/interfaces/entry-feed';
 import { IconProp } from '@fortawesome/fontawesome-svg-core';
-import { Observable } from 'rxjs';
-
 /**
  * Represents a navigation with routing icons for feed
  */
 export interface FeedNav {
   label: string;
   route: string;
-  api: Observable<ApiResponse<EntryFeed>>;
   icon: IconProp;
 }
