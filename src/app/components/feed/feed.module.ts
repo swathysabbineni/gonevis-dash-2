3523fe308b2b68f95e8d6870528aabbaec1fabfe
Amir Savand <amir@savandbros.com>
2019-06-01 12:03:01 +0300

    Fix bookmarked icon and update imports

--- src/app/components/feed/feed.module.ts
+++ src/app/components/feed/feed.module.ts
@@ -2,8 +2,15 @@ import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { library } from '@fortawesome/fontawesome-svg-core';
-import { faBookmark, faComment, faEye, faHeart, faShareSquare } from '@fortawesome/free-regular-svg-icons';
-import { faHeart as faHeartFill, faStar, faHashtag } from '@fortawesome/free-solid-svg-icons';
+import { faBookmark } from '@fortawesome/free-regular-svg-icons/faBookmark';
+import { faComment } from '@fortawesome/free-regular-svg-icons/faComment';
+import { faEye } from '@fortawesome/free-regular-svg-icons/faEye';
+import { faHeart } from '@fortawesome/free-regular-svg-icons/faHeart';
+import { faShareSquare } from '@fortawesome/free-regular-svg-icons/faShareSquare';
+import { faHeart as faHeartFill } from '@fortawesome/free-solid-svg-icons';
+import { faBookmark as faBookmarkFill } from '@fortawesome/free-solid-svg-icons/faBookmark';
+import { faHashtag } from '@fortawesome/free-solid-svg-icons/faHashtag';
+import { faStar } from '@fortawesome/free-solid-svg-icons/faStar';
 import { TranslateModule } from '@ngx-translate/core';
 import { TooltipModule } from 'ngx-bootstrap';
 import { InfiniteScrollModule } from 'ngx-infinite-scroll';
@@ -19,7 +26,7 @@ import { FeedComponent } from './feed.component';
     FontAwesomeModule,
     TranslateModule.forChild(),
     TooltipModule.forRoot(),
-    InfiniteScrollModule
+    InfiniteScrollModule,
   ],
 })
 export class FeedModule {
@@ -29,6 +36,7 @@ export class FeedModule {
     library.add(faComment);
     library.add(faEye);
     library.add(faBookmark);
+    library.add(faBookmarkFill);
     library.add(faShareSquare);
     library.add(faStar);
     library.add(faHashtag);
