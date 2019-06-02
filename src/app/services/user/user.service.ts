098355db0165d5685e1f5ed8bccff045df3ae101
Arsalan Savand <ArsalanSavand@users.noreply.github.com>
2019-06-02 21:08:15 +0300

    Update UserService

--- src/app/services/user/user.service.ts
+++ src/app/services/user/user.service.ts
@@ -1,6 +1,10 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { UserSettings } from '@app/interfaces/userSettings';
+import { UserAuth } from '@app/interfaces/user-auth';
+import { UserSettingsPatch } from '@app/interfaces/user-settings-patch';
 import { ApiService } from '@app/services/api/api.service';
+import { AuthService } from '@app/services/auth/auth.service';
 import { Observable } from 'rxjs';
 
 @Injectable({
@@ -8,14 +12,23 @@ import { Observable } from 'rxjs';
 })
 export class UserService {
 
+  /**
+   * Authenticated user ID
+   */
+  userId: string;
+
   constructor(private http: HttpClient,
-              private apiService: ApiService) {
+              private apiService: ApiService,
+              private authService: AuthService) {
+    this.authService.user.subscribe((data: UserAuth): void => {
+      this.userId = data.id;
+    });
   }
 
   /**
    * Send forgot password link.
    *
-   * @param email User email
+   * @param email UserSettings email
    */
   forgotPassword(email: string): Observable<void> {
     return this.http.post<void>(`${this.apiService.base.v1}account/forgot-password/`, { email });
@@ -28,5 +41,26 @@ export class UserService {
    */
   resetPassword(password: string): Observable<void> {
     return this.http.post<void>(`${this.apiService.base.v1}account/password-reset/`, { password });
+  }
+
+  /**
+   * Get user information
+   */
+  getUser(): Observable<UserSettings> {
+    return this.http.get<UserSettings>(`${this.apiService.base.v1}account/users/${this.userId}/`);
+  }
+
+  updateProfile(
+    about: string,
+    location: string,
+    name: string,
+    ReceiveEmailNotification: boolean,
+  ): Observable<UserSettingsPatch> {
+    return this.http.patch<UserSettingsPatch>(`${this.apiService.base.v1}account/update-profile/`, {
+      about,
+      location,
+      name,
+      receive_email_notification: ReceiveEmailNotification,
+    });
   }
 }
