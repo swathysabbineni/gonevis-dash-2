0264c4053df859157cae193c4cff88cb09c02e36
Arsalan Savand <ArsalanSavand@users.noreply.github.com>
2019-06-02 20:45:46 +0300

    Update UserSettingComponent to update profile

--- src/app/components/user/user-setting/user-setting.component.ts
+++ src/app/components/user/user-setting/user-setting.component.ts
@@ -1,15 +1,69 @@
 import { Component, OnInit } from '@angular/core';
+import { ApiError } from '@app/interfaces/api-error';
+import { User } from '@app/interfaces/user';
+import { UserAuth } from '@app/interfaces/user-auth';
+import { UserSettings } from '@app/interfaces/user-settings';
+import { HttpErrorResponseApi } from '@app/models/http-error-response-api';
+import { AuthService } from '@app/services/auth/auth.service';
+import { UserService } from '@app/services/user/user.service';
 
 @Component({
   selector: 'app-user-setting',
   templateUrl: './user-setting.component.html',
-  styleUrls: ['./user-setting.component.scss']
+  styleUrls: ['./user-setting.component.scss'],
 })
 export class UserSettingComponent implements OnInit {
 
-  constructor() { }
+  user: User;
+  userAuth: UserAuth;
 
-  ngOnInit() {
+  /**
+   * API errors
+   */
+  error: ApiError = {};
+
+  /**
+   * API loading indicator
+   */
+  loading: boolean;
+
+  constructor(private userService: UserService,
+              private authService: AuthService) {
+    this.loading = true;
   }
 
+  ngOnInit(): void {
+    this.authService.user.subscribe((data: UserAuth): void => {
+      this.userAuth = data;
+    });
+    this.userService.getUser().subscribe((data: User): void => {
+      this.loading = false;
+      this.user = data;
+      this.loading = false;
+    });
+  }
+
+  /**
+   * Update user
+   */
+  submit(): void {
+    this.loading = true;
+    this.userService.updateProfile(
+      this.user.about,
+      this.user.location,
+      this.user.name,
+      this.user.receive_email_notification,
+    ).subscribe((data: UserSettings): void => {
+      for (const key in data) {
+        if (this.userAuth.hasOwnProperty(key)) {
+          this.userAuth[key] = data[key];
+        }
+      }
+      this.authService.setAuthenticatedUser(this.userAuth);
+      this.loading = false;
+    }, (error: HttpErrorResponseApi): void => {
+      this.error = error.error;
+      this.loading = false;
+    });
+  }
 }
