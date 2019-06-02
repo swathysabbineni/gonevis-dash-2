879831e876a8d7b394114ef4c7d1af75bd6a3a20
Arsalan Savand <ArsalanSavand@users.noreply.github.com>
2019-06-02 20:44:47 +0300

    Import FormsModule into UserSettingModule

--- src/app/components/user/user-setting/user-setting.module.ts
+++ src/app/components/user/user-setting/user-setting.module.ts
@@ -1,5 +1,6 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { UserSettingRoutingModule } from '@app/components/user/user-setting/user-setting-routing.module';
 import { UserSettingComponent } from '@app/components/user/user-setting/user-setting.component';
 import { PageCoverModule } from '@app/shared/page-cover/page-cover.module';
@@ -10,6 +11,7 @@ import { TranslateModule } from '@ngx-translate/core';
   imports: [
     CommonModule,
     UserSettingRoutingModule,
+    FormsModule,
     TranslateModule.forChild(),
     PageCoverModule,
   ],
