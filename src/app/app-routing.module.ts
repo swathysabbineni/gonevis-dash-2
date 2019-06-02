7d69c8461d2043a12ff291727d424e7f1e4b6f55
Amir Savand <amir@savandbros.com>
2019-06-02 20:19:44 +0300

    Delete blog
    We don't need it for now, not even ready in the back-end.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,9 +2,6 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [{
-  path: 'blog',
-  loadChildren: './components/blog/blog.module#BlogModule',
-}, {
   path: 'feed',
   loadChildren: './components/feed/feed.module#FeedModule',
 }, {
