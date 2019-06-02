f88f9fe1fead8f1eb7643ee60af87901d7f25e25
Arsalan Savand <ArsalanSavand@users.noreply.github.com>
2019-06-02 14:28:17 +0300

    Import needed modules

--- src/app/components/feed/entry/entry.module.ts
+++ src/app/components/feed/entry/entry.module.ts
@@ -1,6 +1,17 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
 import { EntryComponent } from '@app/components/feed/entry/entry.component';
+import { EntryShareModule } from '@app/shared/entry-share/entry-share.module';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { faBookmark } from '@fortawesome/free-regular-svg-icons/faBookmark';
+import { faComment } from '@fortawesome/free-regular-svg-icons/faComment';
+import { faEye } from '@fortawesome/free-regular-svg-icons/faEye';
+import { faHeart } from '@fortawesome/free-regular-svg-icons/faHeart';
+import { faShareSquare } from '@fortawesome/free-regular-svg-icons/faShareSquare';
+import { faHeart as faHeartFill } from '@fortawesome/free-solid-svg-icons';
+import { faBookmark as faBookmarkFill } from '@fortawesome/free-solid-svg-icons/faBookmark';
+import { PopoverModule, TooltipModule } from 'ngx-bootstrap';
 
 import { EntryRoutingModule } from './entry-routing.module';
 
@@ -9,7 +20,20 @@ import { EntryRoutingModule } from './entry-routing.module';
   imports: [
     CommonModule,
     EntryRoutingModule,
+    EntryShareModule,
+    FontAwesomeModule,
+    TooltipModule.forRoot(),
+    PopoverModule.forRoot(),
   ],
 })
 export class EntryModule {
+  constructor() {
+    library.add(faHeart);
+    library.add(faHeartFill);
+    library.add(faComment);
+    library.add(faEye);
+    library.add(faBookmark);
+    library.add(faBookmarkFill);
+    library.add(faShareSquare);
+  }
 }
