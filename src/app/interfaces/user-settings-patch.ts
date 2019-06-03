8c94744271704d59a45e7b66ba90fd3cfd3c6e4b
Arsalan Savand <ArsalanSavand@users.noreply.github.com>
2019-06-03 15:32:28 +0300

    Update UserSettingsPatch

--- src/app/interfaces/user-settings-patch.ts
+++ src/app/interfaces/user-settings-patch.ts
@@ -5,13 +5,14 @@ import { UserTour } from '@app/interfaces/user-tour';
  * Represents user settings structure
  */
 export interface UserSettingsPatch {
-  about: string;
-  email: string;
-  get_absolute_uri: string;
-  has_verified_email: boolean;
-  location: string;
-  media: Media;
-  name: string;
-  receive_email_notification: boolean;
-  tour: UserTour;
+  about?: string;
+  email?: string;
+  get_absolute_uri?: string;
+  has_verified_email?: boolean;
+  location?: string;
+  media?: Media;
+  name?: string;
+  receive_email_notification?: boolean;
+  tour?: UserTour;
+  picture?: FormData | null;
 }
