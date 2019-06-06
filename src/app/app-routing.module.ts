721b14d5a29caec3294397bee854bc3766ad3c26
Arsalan Savand <ArsalanSavand@users.noreply.github.com>
2019-06-06 15:43:45 +0300

    Add canLoad to AppRoutingModule

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AuthGuardService } from '@app/services/auth-guard/auth-guard.service';
 
 const routes: Routes = [{
   path: 'feed',
   loadChildren: './components/feed/feed.module#FeedModule',
+  canLoad: [AuthGuardService],
 }, {
   path: 'user',
   loadChildren: './components/user/user.module#UserModule',
