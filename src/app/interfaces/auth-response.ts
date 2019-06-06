07a36dd51f2e829209bcc9dd954b7c984d7ac627
Amir Savand <amir@savandbros.com>
2019-06-06 19:43:37 +0300

    Add comment

--- src/app/interfaces/auth-response.ts
+++ src/app/interfaces/auth-response.ts
@@ -1,5 +1,8 @@
 import { UserAuth } from '@app/interfaces/user-auth';
 
+/**
+ * Represents authenticaton response data structure
+ */
 export interface AuthResponse {
   token: string;
   user: UserAuth;
