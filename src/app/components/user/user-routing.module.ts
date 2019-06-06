7bd280c165dd04e90ec50c335d18bf4bcf3d7569
Arsalan Savand <ArsalanSavand@users.noreply.github.com>
2019-06-06 15:43:28 +0300

    Add canLoad option to UserRoutingModule

--- src/app/components/user/user-routing.module.ts
+++ src/app/components/user/user-routing.module.ts
@@ -1,5 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AnonymousGuardService } from '@app/services/anonymous-guard/anonymous-guard.service';
+import { AuthGuardService } from '@app/services/auth-guard/auth-guard.service';
 import { UserComponent } from './user.component';
 
 const routes: Routes = [{
@@ -11,12 +13,15 @@ const routes: Routes = [{
   children: [{
     path: 'setting',
     loadChildren: './user-setting/user-setting.module#UserSettingModule',
+    canLoad: [AuthGuardService],
   }, {
     path: 'sign-in',
     loadChildren: './sign-in/sign-in.module#SignInModule',
+    canLoad: [AnonymousGuardService],
   }, {
     path: 'sign-up',
     loadChildren: './sign-up/sign-up.module#SignUpModule',
+    canLoad: [AnonymousGuardService],
   }, {
     path: 'forgot-password',
     loadChildren: './forgot-password/forgot-password.module#ForgotPasswordModule',
