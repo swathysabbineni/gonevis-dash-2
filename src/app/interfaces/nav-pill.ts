839dbbe684dd77f3f7dc093fd5a57b3f97eae572
Arsalan Savand <ArsalanSavand@users.noreply.github.com>
2019-06-07 14:36:33 +0300

    Rename SidebarNav interface to NavPill

--- src/app/interfaces/nav-pill.ts
+++ src/app/interfaces/nav-pill.ts
@@ -1,8 +1,8 @@
 import { IconProp } from '@fortawesome/fontawesome-svg-core';
 /**
- * Represents a sidebar navigation with routing icons
+ * Represents a navigation pill with routing icons
  */
-export interface SidebarNav {
+export interface NavPill {
   label: string;
   route: string;
   icon: IconProp;
