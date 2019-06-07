873a07d9794f35f080b6863c793d1e0b9dd40958
Arsalan Savand <ArsalanSavand@users.noreply.github.com>
2019-06-07 14:36:50 +0300

    Import NavPillsModule into UserSettingModule

--- src/app/components/user/user-setting/user-setting.module.ts
+++ src/app/components/user/user-setting/user-setting.module.ts
@@ -2,6 +2,7 @@ import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { UserSettingRoutingModule } from '@app/components/user/user-setting/user-setting-routing.module';
 import { UserSettingComponent } from '@app/components/user/user-setting/user-setting.component';
+import { NavPillsModule } from '@app/shared/nav-pills/nav-pills.module';
 import { PageCoverModule } from '@app/shared/page-cover/page-cover.module';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { TranslateModule } from '@ngx-translate/core';
@@ -16,6 +17,7 @@ import { TabsModule } from 'ngx-bootstrap';
     TabsModule.forRoot(),
     FontAwesomeModule,
     PageCoverModule,
+    NavPillsModule,
   ],
 })
 export class UserSettingModule {
