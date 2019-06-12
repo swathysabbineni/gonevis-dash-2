ef13ce98af3eaba037c724b3bec7903e5490242f
Amir Savand <amir@savandbros.com>
2019-06-12 14:10:54 +0300

    Add CommentFeed.is_voted

--- src/app/interfaces/comment-feed.ts
+++ src/app/interfaces/comment-feed.ts
@@ -6,6 +6,7 @@ import { UserMinimal } from '@app/interfaces/user-minimal';
 export interface CommentFeed {
   id: string;
   user: UserMinimal;
+  is_voted: boolean;
   comment: string;
   created: string;
   updated: string;
