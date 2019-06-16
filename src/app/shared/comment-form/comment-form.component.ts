dec7674cbf93ccba0daad9a8f09a78a49167cea5
Amir Savand <amir@savandbros.com>
2019-06-16 19:56:20 +0300

    Improve comment form component

--- src/app/shared/comment-form/comment-form.component.ts
+++ src/app/shared/comment-form/comment-form.component.ts
@@ -1,13 +1,4 @@
-import {
-  ChangeDetectorRef,
-  Component,
-  EventEmitter,
-  Input,
-  OnChanges,
-  Output,
-  SimpleChange,
-  SimpleChanges,
-} from '@angular/core';
+import { ChangeDetectorRef, Component, EventEmitter, Input, Output } from '@angular/core';
 import { FormBuilder } from '@angular/forms';
 import { ApiError } from '@app/interfaces/api-error';
 import { CommentFeed } from '@app/interfaces/comment-feed';
@@ -39,22 +30,22 @@ export class CommentFormComponent {
   loading: boolean;
 
   /**
-   * Comment
-   */
-  @Input() comment: string;
-
-  /**
-   * Entry ID
+   * Entry ID of the comment
    */
   @Input() entryId: string;
 
   /**
-   * Comment ID
+   * Comment ID is required for editing
    */
   @Input() commentId: string;
 
   /**
-   * On form submit
+   * Comment content for both editing and creating comment
+   */
+  @Input() commentContent: string;
+
+  /**
+   * Form submission event
    */
   @Output() formSubmitted: EventEmitter<CommentFeed> = new EventEmitter<CommentFeed>();
 
@@ -62,7 +53,6 @@ export class CommentFormComponent {
               private changeDetectorRef: ChangeDetectorRef,
               private authService: AuthService,
               private commentService: CommentService) {
-
     /**
      * Get authenticated user data
      */
@@ -87,9 +77,9 @@ export class CommentFormComponent {
    */
   submit(): void {
     this.loading = true;
-    this.commentService.comment(this.entryId, this.comment).subscribe((data: CommentFeed): void => {
+    this.commentService.comment(this.entryId, this.commentContent).subscribe((data: CommentFeed): void => {
       this.formSubmitted.next(data);
-      this.comment = '';
+      this.commentContent = '';
       this.loading = false;
       this.errors = {};
     }, (error: HttpErrorResponseApi): void => {
@@ -103,9 +93,9 @@ export class CommentFormComponent {
    */
   edit(): void {
     this.loading = true;
-    this.commentService.edit(this.commentId, this.comment).subscribe((data: CommentFeed): void => {
+    this.commentService.edit(this.commentId, this.commentContent).subscribe((data: CommentFeed): void => {
       this.formSubmitted.next(data);
-      this.comment = data.comment;
+      this.commentContent = data.comment;
       this.loading = false;
       this.errors = {};
     }, (error: HttpErrorResponseApi): void => {
