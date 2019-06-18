3dc108528ae6b5012bba258019a1018a26ca9a66
Arsalan Savand <ArsalanSavand@users.noreply.github.com>
2019-06-18 10:43:51 +0300

    Add documentation to CommentFormEvent

--- src/app/interfaces/comment-form-event.ts
+++ src/app/interfaces/comment-form-event.ts
@@ -1,5 +1,8 @@
 import { CommentFeed } from '@app/interfaces/comment-feed';
 
+/**
+ * Represents comment from emitted event structure
+ */
 export interface CommentFormEvent {
   comment: CommentFeed;
   isEdit: boolean;
