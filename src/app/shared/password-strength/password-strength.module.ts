91f8823a2b8cc2bfb91c1ce93fa164c0cc7dcf78
Arsalan Savand <ArsalanSavand@users.noreply.github.com>
2019-06-24 15:09:16 +0300

    Add PasswordStrengthComponent shared

---
+++ src/app/shared/password-strength/password-strength.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { PasswordStrengthComponent } from '@app/shared/password-strength/password-strength.component';
+import { ProgressbarModule } from 'ngx-bootstrap';
+
+@NgModule({
+  declarations: [PasswordStrengthComponent],
+  imports: [
+    CommonModule,
+    ProgressbarModule.forRoot(),
+  ],
+  exports: [PasswordStrengthComponent],
+})
+export class PasswordStrengthModule {
+}
