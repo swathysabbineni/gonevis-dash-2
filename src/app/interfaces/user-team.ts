ea5252297edd39bed06816e27bc437af20e39696
Arsalan Savand <ArsalanSavand@users.noreply.github.com>
2019-06-25 15:18:17 +0300

    Add UserTeam interface

---
+++ src/app/interfaces/user-team.ts
@@ -0,0 +1,13 @@
+import { Media } from '@app/interfaces/media';
+
+/**
+ * Represents team member user data
+ */
+export interface UserTeam {
+  email: string;
+  get_absolute_uri: string;
+  get_full_name: string;
+  media: Media;
+  name: string;
+  username: string;
+}
