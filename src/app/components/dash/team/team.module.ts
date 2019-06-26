607d47ae3cf3baf28ca7d0602b1765f9ce785af5
Arsalan Savand <ArsalanSavand@users.noreply.github.com>
2019-06-26 11:04:48 +0300

    Import 'BsDropdownModule' module

--- src/app/components/dash/team/team.module.ts
+++ src/app/components/dash/team/team.module.ts
@@ -3,6 +3,7 @@ import { CommonModule } from '@angular/common';
 import { ReactiveFormsModule } from '@angular/forms';
 import { TeamComponent } from '@app/components/dash/team/team.component';
 import { TranslateModule } from '@ngx-translate/core';
+import { BsDropdownModule } from 'ngx-bootstrap';
 
 import { TeamRoutingModule } from './team-routing.module';
 
@@ -13,6 +14,7 @@ import { TeamRoutingModule } from './team-routing.module';
     TeamRoutingModule,
     ReactiveFormsModule,
     TranslateModule.forChild(),
+    BsDropdownModule.forRoot()
   ],
 })
 export class TeamModule {
