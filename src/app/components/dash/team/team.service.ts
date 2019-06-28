ee9cc24efcba880835df69bf830b56dffbe8b3d9
Arsalan Savand <ArsalanSavand@users.noreply.github.com>
2019-06-28 12:58:42 +0300

    Add new method to TeamService to remove a member

--- src/app/components/dash/team/team.service.ts
+++ src/app/components/dash/team/team.service.ts
@@ -1,10 +1,10 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { TeamRoles } from '@app/enums/team-roles';
+import { Params } from '@app/interfaces/params';
 import { Team } from '@app/interfaces/team';
 import { ApiService } from '@app/services/api/api.service';
 import { BlogService } from '@app/services/blog/blog.service';
-import { TranslateService } from '@ngx-translate/core';
 import { Observable } from 'rxjs';
 
 @Injectable({
@@ -24,7 +24,7 @@ export class TeamService {
   }
 
   /**
-   * Invite new member
+   * Invite new member or promote a member
    *
    * @param email Email address
    * @param role Member's role in blog
@@ -34,5 +34,21 @@ export class TeamService {
       email,
       role,
     });
+  }
+
+  /**
+   * Remove member from team
+   *
+   * @param payload Payload required to remove member
+   * @param isPending Determines whether member is pending or not
+   */
+  removeMember(payload: Params, isPending: boolean): Observable<void> {
+    const blogId: string = BlogService.currentBlog.id;
+    let endpoint = 'remove-team-member';
+    // Check if member is pending
+    if (isPending) {
+      endpoint = 'remove-pending-member';
+    }
+    return this.http.put<void>(`${this.apiService.base.v1}website/site/${blogId}/${endpoint}/`, payload);
   }
 }
