00d4bf67fc4554143c53783ff371f38e20d90d66
Amir Savand <amir@savandbros.com>
2019-08-03 12:22:18 +0300

    CircleCI (#16)
    CircleCI Deployment

---
+++ scripts/deploy.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+if ! [ -x "$(command -v rsync)" ]; then
+  echo 'Error: rsync is not installed.' >&2
+  exit 1
+fi
+
+npm run build-prod
+ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null $DEPLOY_USER@$DEPLOY_HOST -p$DEPLOY_SSH_PORT 'rm -rf public_html/dash2.gonevis.com/*'
+rsync -avz -e "ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -p$DEPLOY_SSH_PORT" --progress .htaccess dist/gonevis/* $DEPLOY_USER@$DEPLOY_HOST:$DEPLOY_PATH
