1c5e788cf6f1ccea6630261bea7c03d4640b1081
Amir Savand <amir@savandbros.com>
2019-08-10 15:43:24 +0300

    Update feed.module.ts

--- src/app/components/feed/feed.module.ts
+++ src/app/components/feed/feed.module.ts
@@ -1,6 +1,5 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
-import { EntryListModule } from '@app/shared/entry-list/entry-list.module';
 import { NavPillsModule } from '@app/shared/nav-pills/nav-pills.module';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { library } from '@fortawesome/fontawesome-svg-core';
@@ -12,13 +11,14 @@ import { FeedRoutingModule } from './feed-routing.module';
 import { FeedComponent } from './feed.component';
 
 @NgModule({
-  declarations: [FeedComponent],
+  declarations: [
+    FeedComponent,
+  ],
   imports: [
     CommonModule,
     FeedRoutingModule,
     FontAwesomeModule,
     TranslateModule.forChild(),
-    EntryListModule,
     NavPillsModule,
   ],
 })
