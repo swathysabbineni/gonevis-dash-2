5d2e18d33a24f5e698baea9778e5018bf94d1aba
Amir Savand <amir@savandbros.com>
2019-08-13 00:09:35 +0300

    Update object-type.ts

--- src/app/enums/object-type.ts
+++ src/app/enums/object-type.ts
@@ -2,10 +2,11 @@
  * Object types used for referencing
  */
 export enum ObjectType {
-  ENTRY = 1,
-  COMMENT = 2,
-  TAG = 3,
-  USER = 4,
-  SITE = 5,
-  DRAFT_ENTRY = 6,
+  None,
+  Entry,
+  Comment,
+  Tag,
+  User,
+  Site,
+  DraftEntry,
 }
