b7b88c735a2b1c6c1c43a97559f81a4842743a06
Amir Savand <amir@savandbros.com>
2019-08-12 20:37:14 +0300

    Fix entry share component

--- src/app/shared/entry-share/entry-share.component.ts
+++ src/app/shared/entry-share/entry-share.component.ts
@@ -1,21 +1,27 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, ElementRef, ViewChild } from '@angular/core';
+import { Entry } from '@app/interfaces/zero/entry';
 
 @Component({
   selector: 'app-entry-share',
   templateUrl: './entry-share.component.html',
   styleUrls: ['./entry-share.component.scss'],
 })
-export class EntryShareComponent implements OnInit {
+export class EntryShareComponent {
+
+  @ViewChild('shareInput', { static: true }) shareInput: ElementRef;
 
   /**
-   * Share URL
+   * Entry to share URL for
    */
-  @Input() url: string;
+  @Input() entry: Entry;
 
   constructor() {
   }
 
-  ngOnInit() {
+  /**
+   * @returns Full URL address of this entry
+   */
+  get url(): string {
+    return `${location.origin}/feed/entry/${this.entry.id}`;
   }
-
 }
