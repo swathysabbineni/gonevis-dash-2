f7ddb027835eeadb4849234fc6170013701cab06
Amir Savand <amir@savandbros.com>
2019-08-13 21:06:56 +0300

    New design for tags

--- src/app/components/dash/tags/tags.module.ts
+++ src/app/components/dash/tags/tags.module.ts
@@ -1,5 +1,10 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { faEdit } from '@fortawesome/free-solid-svg-icons/faEdit';
+import { faEye } from '@fortawesome/free-solid-svg-icons/faEye';
+import { faTrash } from '@fortawesome/free-solid-svg-icons/faTrash';
 
 import { TagsRoutingModule } from './tags-routing.module';
 import { TagsComponent } from './tags.component';
@@ -11,7 +16,13 @@ import { TagsComponent } from './tags.component';
   imports: [
     CommonModule,
     TagsRoutingModule,
+    FontAwesomeModule,
   ],
 })
 export class TagsModule {
+  constructor() {
+    library.add(faEdit);
+    library.add(faTrash);
+    library.add(faEye);
+  }
 }
