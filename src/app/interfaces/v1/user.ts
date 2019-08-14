8de2f2ddf075cce86ce46b834aeb3854eed672e4
Amir Savand <amir@savandbros.com>
2019-08-14 19:14:59 +0300

    Create user.ts

---
+++ src/app/interfaces/v1/user.ts
@@ -0,0 +1,18 @@
+import { Media } from '@app/interfaces/media';
+
+export interface User {
+  id: string;
+  username: string;
+  name: string;
+  location: string;
+  about: string;
+  is_active: boolean;
+  date_joined: Date;
+  updated: Date;
+  media: Media;
+  get_full_name: string;
+  get_short_name: string;
+  get_absolute_uri: string;
+  receive_email_notification: boolean;
+  has_verified_email: boolean;
+}
