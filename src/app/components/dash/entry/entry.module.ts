a028af7a3c1f8e3a96f70f684e86e348bf8ae975
Amir Savand <amir@savandbros.com>
2019-08-18 16:52:19 +0300

    Design entry page

--- src/app/components/dash/entry/entry.module.ts
+++ src/app/components/dash/entry/entry.module.ts
@@ -1,5 +1,11 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { faComment } from '@fortawesome/free-regular-svg-icons/faComment';
+import { faEye } from '@fortawesome/free-regular-svg-icons/faEye';
+import { faThumbsUp } from '@fortawesome/free-regular-svg-icons/faThumbsUp';
+import { faEllipsisV } from '@fortawesome/free-solid-svg-icons/faEllipsisV';
 import { TranslateModule } from '@ngx-translate/core';
 
 import { EntryRoutingModule } from './entry-routing.module';
@@ -13,7 +19,14 @@ import { EntryComponent } from './entry.component';
     CommonModule,
     EntryRoutingModule,
     TranslateModule.forChild(),
+    FontAwesomeModule,
   ],
 })
 export class EntryModule {
+  constructor() {
+    library.add(faComment);
+    library.add(faThumbsUp);
+    library.add(faEye);
+    library.add(faEllipsisV);
+  }
 }
