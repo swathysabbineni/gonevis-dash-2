7127f9e9e12019df6b363f10a4ef10272501ce25
Amir Savand <amir@savandbros.com>
2019-08-18 16:14:51 +0300

    Create entry.ts

---
+++ src/app/interfaces/v1/entry.ts
@@ -0,0 +1,36 @@
+import { Media } from '@app/interfaces/media';
+import { TagMin } from '@app/interfaces/v1/tag-min';
+import { UserMin } from '@app/interfaces/v1/user-min';
+
+export interface Entry {
+  id: string;
+  tags: TagMin[];
+  media: Media;
+  absolute_uri: string;
+  site: string;
+  circles: any[];
+  updated_by: UserMin;
+  entrydraft?: any;
+  vote_count: number;
+  title: string;
+  slug: string;
+  excerpt: string;
+  content: string;
+  meta_description?: string;
+  status: number;
+  format: number;
+  comment_enabled: boolean;
+  featured: boolean;
+  is_page: boolean;
+  password: string;
+  view_count: number;
+  published: string;
+  start_publication?: string;
+  comment_count: number;
+  active_comment_count: number;
+  hidden_comment_count: number;
+  pending_comment_count: number;
+  created: string;
+  updated: string;
+  user: string;
+}
