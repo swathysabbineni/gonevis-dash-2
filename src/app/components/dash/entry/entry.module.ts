932fe6fa5eae647d2b05f02dd0228a8f8f88e820
Amer Ansari <amer@savandbros.com>
2019-08-19 03:08:46 +0400

    Update entry.module.ts

--- src/app/components/dash/entry/entry.module.ts
+++ src/app/components/dash/entry/entry.module.ts
@@ -1,6 +1,13 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { faComment } from '@fortawesome/free-regular-svg-icons/faComment';
+import { faEye } from '@fortawesome/free-regular-svg-icons/faEye';
+import { faThumbsUp } from '@fortawesome/free-regular-svg-icons/faThumbsUp';
+import { faEllipsisV } from '@fortawesome/free-solid-svg-icons/faEllipsisV';
 import { TranslateModule } from '@ngx-translate/core';
+import { BsDropdownModule } from 'ngx-bootstrap';
 
 import { EntryRoutingModule } from './entry-routing.module';
 import { EntryComponent } from './entry.component';
@@ -13,7 +20,15 @@ import { EntryComponent } from './entry.component';
     CommonModule,
     EntryRoutingModule,
     TranslateModule.forChild(),
+    FontAwesomeModule,
+    BsDropdownModule,
   ],
 })
 export class EntryModule {
+  constructor() {
+    library.add(faComment);
+    library.add(faThumbsUp);
+    library.add(faEye);
+    library.add(faEllipsisV);
+  }
 }
