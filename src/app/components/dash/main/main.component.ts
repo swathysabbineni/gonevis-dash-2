ff06dd9ce3b50ad61ace8f22ff979d264a5f80f0
Amer Ansari <amer@savandbros.com>
2019-08-23 06:22:58 +0400

    Show posts

--- src/app/components/dash/main/main.component.ts
+++ src/app/components/dash/main/main.component.ts
@@ -1,7 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 import { CommentsService } from '@app/components/dash/comments/comments.service';
+import { EntryService } from '@app/components/dash/entry/entry.service';
 import { ApiResponse } from '@app/interfaces/api-response';
 import { Comment } from '@app/interfaces/v1/comment';
+import { Entry } from '@app/interfaces/v1/entry';
 
 @Component({
   selector: 'app-main',
@@ -9,6 +12,12 @@ import { Comment } from '@app/interfaces/v1/comment';
   styleUrls: ['./main.component.scss'],
 })
 export class MainComponent implements OnInit {
+
+  /**
+   * Is showing posts or pages
+   * @see DashRoutingModule
+   */
+  readonly isPages: boolean = this.route.snapshot.data.pages === true;
 
   /**
    * List of comments
@@ -20,7 +29,19 @@ export class MainComponent implements OnInit {
    */
   commentsCount = 0;
 
-  constructor(private commentsService: CommentsService) {
+  /**
+   * List of entries
+   */
+  entries: Entry[];
+
+  /**
+   * Entries count
+   */
+  entriesCount = 0;
+
+  constructor(private commentsService: CommentsService,
+              private entryService: EntryService,
+              private route: ActivatedRoute) {
   }
 
   ngOnInit(): void {
@@ -30,6 +51,15 @@ export class MainComponent implements OnInit {
     this.commentsService.getComments().subscribe((response: ApiResponse<Comment>): void => {
       this.comments = response.results;
       this.commentsCount = response.count;
+    });
+    /**
+     * Load entries
+     */
+    this.entryService.getEntries({
+      is_page: this.isPages,
+    }).subscribe((response: ApiResponse<Entry>): void => {
+      this.entries = response.results;
+      this.entriesCount = response.count;
     });
   }
 }
--- src/app/components/dash/main/main.module.ts
+++ src/app/components/dash/main/main.module.ts
@@ -1,5 +1,10 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { faComment } from '@fortawesome/free-regular-svg-icons/faComment';
+import { faEye } from '@fortawesome/free-regular-svg-icons/faEye';
+import { faThumbsUp } from '@fortawesome/free-regular-svg-icons/faThumbsUp';
 import { TranslateModule } from '@ngx-translate/core';
 
 import { MainRoutingModule } from './main-routing.module';
@@ -13,7 +18,13 @@ import { MainComponent } from './main.component';
     CommonModule,
     MainRoutingModule,
     TranslateModule,
+    FontAwesomeModule,
   ],
 })
 export class MainModule {
+  constructor() {
+    library.add(faComment);
+    library.add(faThumbsUp);
+    library.add(faEye);
+  }
 }
