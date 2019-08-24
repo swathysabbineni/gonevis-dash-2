de9d0f7353f6aa6071712ec607051c721cf3e9f9
Amir Savand <amir@savandbros.com>
2019-08-24 11:41:48 +0300

    Create blog-settings.ts

---
+++ src/app/interfaces/v1/blog-settings.ts
@@ -0,0 +1,33 @@
+import { HighlightTheme } from '@app/enums/highlight-theme';
+import { TemplatePrimaryColor } from '@app/enums/template-primary-color';
+import { File } from '@app/interfaces/file';
+
+export interface BlogSettings {
+  title: string;
+  description: string;
+  meta_description?: string;
+  highlight_theme: HighlightTheme;
+  template_primary_color: TemplatePrimaryColor;
+  paginate_by: number;
+  commenting: boolean;
+  voting: boolean;
+  media: {
+    cover_image: File;
+    logo: File;
+  };
+  url: string;
+  domains: {
+    id: number;
+    domain: string;
+    cname: string;
+  }[];
+  search_engine_visibility: boolean;
+  absolute_uri: string;
+  remove_branding: boolean;
+  footer_text?: string;
+  google_analytics_code?: string;
+  google_analytics_enabled: boolean;
+  google_adsense_code?: string;
+  google_adsense_enabled: boolean;
+  show_views_count: boolean;
+}
