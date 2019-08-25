7a3413d3f8719c68fc4bd4beeaf3426749bab2a0
Amir Savand <amir@savandbros.com>
2019-08-25 19:23:51 +0300

    Add settings > appearance > media

--- src/app/components/dash/settings/settings-appearance/settings-appearance.component.ts
+++ src/app/components/dash/settings/settings-appearance/settings-appearance.component.ts
@@ -1,15 +1,38 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder } from '@angular/forms';
+import { ApiError } from '@app/interfaces/api-error';
+import { BlogSettings } from '@app/interfaces/v1/blog-settings';
+import { Domain } from '@app/interfaces/v1/domain';
+import { BlogService } from '@app/services/blog/blog.service';
+import { TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-settings-appearance',
   templateUrl: './settings-appearance.component.html',
-  styleUrls: ['./settings-appearance.component.scss']
+  styleUrls: ['./settings-appearance.component.scss'],
 })
 export class SettingsAppearanceComponent implements OnInit {
 
-  constructor() { }
 
-  ngOnInit() {
+  /**
+   * Blog settings data
+   */
+  settings: BlogSettings;
+
+  constructor(private formBuilder: FormBuilder,
+              private blogService: BlogService) {
   }
 
+  ngOnInit(): void {
+    this.getSettings();
+  }
+
+  /**
+   * Get blog settings
+   */
+  getSettings(): void {
+    this.blogService.getSettings(BlogService.currentBlog.id).subscribe((data: BlogSettings): void => {
+      this.settings = data;
+    });
+  }
 }
