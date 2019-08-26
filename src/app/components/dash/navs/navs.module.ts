a82f7ec206a7f35edd6e1889135be9954fd1649e
Amer Ansari <amer@savandbros.com>
2019-08-26 12:16:58 +0400

    Update navs.module.ts

--- src/app/components/dash/navs/navs.module.ts
+++ src/app/components/dash/navs/navs.module.ts
@@ -1,5 +1,13 @@
+import { DragDropModule } from '@angular/cdk/drag-drop';
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { faGripLinesVertical } from '@fortawesome/free-solid-svg-icons/faGripLinesVertical';
+import { faPlus } from '@fortawesome/free-solid-svg-icons/faPlus';
+import { faTrash } from '@fortawesome/free-solid-svg-icons/faTrash';
+import { TranslateModule } from '@ngx-translate/core';
 
 import { NavsRoutingModule } from './navs-routing.module';
 import { NavsComponent } from './navs.component';
@@ -11,7 +19,17 @@ import { NavsComponent } from './navs.component';
   imports: [
     CommonModule,
     NavsRoutingModule,
+    ReactiveFormsModule,
+    TranslateModule,
+    FontAwesomeModule,
+    FormsModule,
+    DragDropModule,
   ],
 })
 export class NavsModule {
+  constructor() {
+    library.add(faGripLinesVertical);
+    library.add(faPlus);
+    library.add(faTrash);
+  }
 }
