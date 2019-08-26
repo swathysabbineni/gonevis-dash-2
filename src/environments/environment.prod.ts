7363ca050a27e79312d9d8856e50300828db7216
Amir Savand <amir@savandbros.com>
2019-08-26 20:23:41 +0300

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,7 @@
-export const environment = {
-  production: true,
+import { Environment } from '@app/interfaces/environment';
+
+export const environment: Environment = {
+  development: true,
   api: {
     v1: 'https://www.gonevis.com/api/v1/',
     zero: 'https://www.gonevis.com/api/zero/',
