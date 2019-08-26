ceced67811a823aade94b22ed5325e407c6f0cab
Amer Ansari <amer@savandbros.com>
2019-08-26 12:19:10 +0400

    Add navigations page

--- src/app/components/dash/navs/navs.component.ts
+++ src/app/components/dash/navs/navs.component.ts
@@ -1,15 +1,86 @@
+import { CdkDragDrop, moveItemInArray } from '@angular/cdk/drag-drop';
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { NavsService } from '@app/components/dash/navs/navs.service';
+import { Nav } from '@app/interfaces/v1/nav';
 
 @Component({
   selector: 'app-navs',
   templateUrl: './navs.component.html',
-  styleUrls: ['./navs.component.scss']
+  styleUrls: ['./navs.component.scss'],
 })
 export class NavsComponent implements OnInit {
 
-  constructor() { }
+  /**
+   * Navigation list
+   */
+  navigations: Nav[] = [];
 
-  ngOnInit() {
+  /**
+   * API loading indicator
+   */
+  loading: boolean;
+
+  constructor(private router: Router,
+              private activatedRoute: ActivatedRoute,
+              private navsService: NavsService) {
   }
 
+  ngOnInit(): void {
+    this.loading = true;
+    /**
+     * Get navigations
+     */
+    this.navsService.getNavs().subscribe((data: { navigation: Nav[] }): void => {
+      this.navigations = data.navigation;
+      this.loading = false;
+
+      /**
+       * Subscribe to route's query params
+       */
+      if (history.state.add) {
+        this.navigations.push({
+          label: history.state.add.label,
+          url: history.state.add.url,
+        });
+      }
+    });
+  }
+
+  /**
+   * Update navigations
+   */
+  update(): void {
+    this.loading = true;
+    /**
+     * Update each navigation's sort number by their indexes
+     */
+    this.navigations.map<void>((navigation: Nav, index: number): void => {
+      navigation.sort_number = index;
+    });
+    this.navsService.update(this.navigations).subscribe((data: any): void => {
+      this.navigations = data.navigation;
+      this.loading = false;
+    });
+  }
+
+  /**
+   * Create new navigation
+   */
+  add(): void {
+    this.navigations.push({
+      label: 'New nav',
+      url: '/',
+      sort_number: this.navigations.length + 1,
+    });
+  }
+
+  /**
+   * On navigation drop callback
+   *
+   * @param event On drop event
+   */
+  drop(event: CdkDragDrop<Nav[]>): void {
+    moveItemInArray(this.navigations, event.previousIndex, event.currentIndex);
+  }
 }
