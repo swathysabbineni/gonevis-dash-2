3bfef33a56ec2d81993a65c6a4860ba006260024
Amir Savand <amir@savandbros.com>
2019-08-26 19:56:31 +0300

    Start settings appearance

--- src/app/components/dash/settings/settings-appearance/settings-appearance.component.ts
+++ src/app/components/dash/settings/settings-appearance/settings-appearance.component.ts
@@ -1,15 +1,65 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder } from '@angular/forms';
+import { HighlightTheme } from '@app/enums/highlight-theme';
+import { TemplatePrimaryColor } from '@app/enums/template-primary-color';
+import { BlogSettings } from '@app/interfaces/v1/blog-settings';
+import { Template } from '@app/interfaces/v1/template';
+import { BlogService } from '@app/services/blog/blog.service';
 
 @Component({
   selector: 'app-settings-appearance',
   templateUrl: './settings-appearance.component.html',
-  styleUrls: ['./settings-appearance.component.scss']
+  styleUrls: ['./settings-appearance.component.scss'],
 })
 export class SettingsAppearanceComponent implements OnInit {
 
-  constructor() { }
+  /**
+   * @see BlogService.highlightThemes
+   */
+  readonly templatePrimaryColors: {
+    id: TemplatePrimaryColor;
+    label: string;
+    color: string
+  }[] = BlogService.templatePrimaryColor;
 
-  ngOnInit() {
+  /**
+   * @see BlogService.highlightThemes
+   */
+  readonly highlightThemes: {
+    id: HighlightTheme;
+    label: string
+  }[] = BlogService.highlightThemes;
+
+  /**
+   * Blog settings data
+   */
+  settings: BlogSettings;
+
+  /**
+   * Available templates for blogs
+   */
+  templates: Template[];
+
+  constructor(private formBuilder: FormBuilder,
+              private blogService: BlogService) {
   }
 
+  ngOnInit(): void {
+    this.getSettings();
+    /**
+     * Get templates
+     */
+    this.blogService.getTemplates().subscribe((data: { templates: Template[] }): void => {
+      this.templates = data.templates;
+    });
+  }
+
+  /**
+   * Get blog settings
+   */
+  getSettings(): void {
+    this.blogService.getSettings().subscribe((data: BlogSettings): void => {
+      this.settings = data;
+    });
+  }
 }
