a421724772c2bb415f850a2ebd766a3e4b65a38a
Arsalan Savand <arsalan@savandbros.com>
2019-08-27 13:47:24 +0300

    Import QuillModule and FormsModule into WriteModule

--- src/app/components/dash/write/write.module.ts
+++ src/app/components/dash/write/write.module.ts
@@ -1,5 +1,7 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { QuillModule } from 'ngx-quill';
 
 import { WriteRoutingModule } from './write-routing.module';
 import { WriteComponent } from './write.component';
@@ -11,6 +13,8 @@ import { WriteComponent } from './write.component';
   imports: [
     CommonModule,
     WriteRoutingModule,
+    QuillModule.forRoot(),
+    FormsModule,
   ],
 })
 export class WriteModule {
