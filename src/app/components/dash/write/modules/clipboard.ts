244ae9cafddddb94665d1bfcee312afc536471ac
Arsalan Savand <arsalan@savandbros.com>
2019-08-27 13:46:58 +0300

    Add Clipboard module

---
+++ src/app/components/dash/write/modules/clipboard.ts
@@ -0,0 +1,26 @@
+import Quill, { BoundsStatic, Delta as DeltaInterface, RangeStatic } from 'quill';
+import Delta from 'quill-delta';
+
+const ClipboardModule: any = Quill.import('modules/clipboard');
+
+class CustomClipboard extends ClipboardModule {
+  onPaste(e: ClipboardEvent): void {
+    if (e.defaultPrevented || !this.quill.isEnabled()) {
+      return;
+    }
+    const range: RangeStatic = this.quill.getSelection();
+    let delta: DeltaInterface = new Delta().retain(range.index);
+    this.container.style.top = (window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop || 0).toString() + 'px';
+    this.container.focus();
+    setTimeout((): void => {
+      this.quill.selection.update('silent');
+      delta = delta.concat(this.convert()).delete(range.length);
+      this.quill.updateContents(delta, 'user');
+      this.quill.setSelection(delta.length() - range.length, range.length, 'silent');
+      const bounds: BoundsStatic = this.quill.getBounds(delta.length() - range.length, range.length);
+      this.quill.scrollingContainer.scrollTop = bounds.top;
+    }, 1);
+  }
+}
+
+Quill.register('modules/clipboard', CustomClipboard, true);
