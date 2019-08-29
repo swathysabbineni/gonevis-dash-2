9df1f919c32d74352943e793f4fb9ffcc9c6c3e6
Amer Ansari <amer@savandbros.com>
2019-08-29 16:27:06 +0400

    Update main.module.ts

--- src/app/components/dash/main/main.module.ts
+++ src/app/components/dash/main/main.module.ts
@@ -1,11 +1,14 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { UsersModalComponent } from '@app/shared/users-modal/users-modal.component';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { library } from '@fortawesome/fontawesome-svg-core';
 import { faComment } from '@fortawesome/free-regular-svg-icons/faComment';
 import { faEye } from '@fortawesome/free-regular-svg-icons/faEye';
 import { faThumbsUp } from '@fortawesome/free-regular-svg-icons/faThumbsUp';
+import { faTimes } from '@fortawesome/free-solid-svg-icons/faTimes';
 import { TranslateModule } from '@ngx-translate/core';
+import { BsModalRef, ModalModule } from 'ngx-bootstrap';
 import { NgMathPipesModule } from 'ngx-pipes';
 
 import { MainRoutingModule } from './main-routing.module';
@@ -14,6 +17,7 @@ import { MainComponent } from './main.component';
 @NgModule({
   declarations: [
     MainComponent,
+    UsersModalComponent,
   ],
   imports: [
     CommonModule,
@@ -21,10 +25,15 @@ import { MainComponent } from './main.component';
     TranslateModule,
     FontAwesomeModule,
     NgMathPipesModule,
+    ModalModule.forRoot(),
+  ],
+  entryComponents: [
+    UsersModalComponent,
   ],
 })
 export class MainModule {
   constructor() {
+    library.add(faTimes)
     library.add(faComment);
     library.add(faThumbsUp);
     library.add(faEye);
