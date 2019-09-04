4205a8bc56974c1a23f3929198b096be22301bb7
Amir Savand <amir@savandbros.com>
2019-09-04 21:11:01 +0300

    Fix imports

--- src/app/components/dash/tags/tags.module.ts
+++ src/app/components/dash/tags/tags.module.ts
@@ -1,6 +1,8 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { ReactiveFormsModule } from '@angular/forms';
+import { TagsModalComponent } from '@app/shared/tags-modal/tags-modal.component';
+import { TagsModalModule } from '@app/shared/tags-modal/tags-modal.module';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { library } from '@fortawesome/fontawesome-svg-core';
 import { faBars } from '@fortawesome/free-solid-svg-icons/faBars';
@@ -8,6 +10,7 @@ import { faEdit } from '@fortawesome/free-solid-svg-icons/faEdit';
 import { faEye } from '@fortawesome/free-solid-svg-icons/faEye';
 import { faTrash } from '@fortawesome/free-solid-svg-icons/faTrash';
 import { TranslateModule } from '@ngx-translate/core';
+import { ModalModule } from 'ngx-bootstrap';
 
 import { TagsRoutingModule } from './tags-routing.module';
 import { TagsComponent } from './tags.component';
@@ -22,6 +25,8 @@ import { TagsComponent } from './tags.component';
     FontAwesomeModule,
     TranslateModule,
     ReactiveFormsModule,
+    ModalModule.forRoot(),
+    TagsModalModule,
   ],
 })
 export class TagsModule {
