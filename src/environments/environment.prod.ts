b7780bd51a56e418e368f9c6a00627c470c0b100
Amir Savand <amir@savandbros.com>
2019-09-08 09:28:01 +0300

    Add Environment.name

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,7 @@
 import { Environment } from '@app/interfaces/environment';
 
 export const environment: Environment = {
+  name: 'production',
   development: true,
   api: {
     v1: 'https://www.gonevis.com/api/v1/',
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,7 @@
 import { Environment } from '@app/interfaces/environment';
 
 export const environment: Environment = {
+  name: 'local',
   development: true,
   api: {
     v1: 'http://gonevis.local:8000/api/v1/',
