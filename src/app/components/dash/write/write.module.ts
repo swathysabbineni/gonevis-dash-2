32dc8bfc99937d46f0aedf1118884c763c59a8a4
Arsalan Savand <arsalan@savandbros.com>
2019-09-12 13:26:56 +0300

    Add MediaSelection modal and import TranslationModule

--- src/app/components/dash/write/write.module.ts
+++ src/app/components/dash/write/write.module.ts
@@ -2,6 +2,7 @@ import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { FileListModule } from '@app/shared/file-list/file-list.module';
+import { TranslateModule } from '@ngx-translate/core';
 import { PopoverModule, ModalModule } from 'ngx-bootstrap';
 import { QuillModule } from 'ngx-quill';
 
@@ -18,6 +19,7 @@ import { WriteComponent } from './write.component';
     WriteRoutingModule,
     FormsModule,
     ReactiveFormsModule,
+    TranslateModule.forChild(),
     QuillModule.forRoot(),
     PopoverModule.forRoot(),
     FileListModule,
