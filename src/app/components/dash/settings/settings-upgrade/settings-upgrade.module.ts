a44d1c7dc78b18c290a20f81b08b1401d08f81a7
Arsalan Savand <arsalan@savandbros.com>
2019-09-18 13:52:39 +0300

    Import needed modules

--- src/app/components/dash/settings/settings-upgrade/settings-upgrade.module.ts
+++ src/app/components/dash/settings/settings-upgrade/settings-upgrade.module.ts
@@ -1,11 +1,13 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { PaymentValidationModule } from '@app/components/dash/settings/settings-upgrade/payment-validation/payment-validation.module';
 import { SettingsUpgradeComponent } from '@app/components/dash/settings/settings-upgrade/settings-upgrade.component';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { library } from '@fortawesome/fontawesome-svg-core';
 import { faDollarSign } from '@fortawesome/free-solid-svg-icons/faDollarSign';
 import { faStar } from '@fortawesome/free-solid-svg-icons/faStar';
 import { TranslateModule } from '@ngx-translate/core';
+import { ModalModule } from 'ngx-bootstrap/modal';
 
 import { SettingsUpgradeRoutingModule } from './settings-upgrade-routing.module';
 
@@ -16,7 +18,9 @@ import { SettingsUpgradeRoutingModule } from './settings-upgrade-routing.module'
     CommonModule,
     SettingsUpgradeRoutingModule,
     FontAwesomeModule,
-    TranslateModule,
+    TranslateModule.forChild(),
+    ModalModule.forRoot(),
+    PaymentValidationModule,
   ],
 })
 export class SettingsUpgradeModule {
