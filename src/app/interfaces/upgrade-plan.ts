a99a0adfeab3b51d8cb8fff85f37467497c95de1
Arsalan Savand <arsalan@savandbros.com>
2019-09-24 14:35:20 +0300

    Add documentation to Upgrade plan

--- src/app/interfaces/upgrade-plan.ts
+++ src/app/interfaces/upgrade-plan.ts
@@ -1,5 +1,8 @@
 import { Plan } from '@app/interfaces/v1/plan';
 
+/**
+ * Payment plan which is used in upgrade page, main purpose is to store static variables for UI
+ */
 export interface UpgradePlan {
   name: string;
   sub: string;
