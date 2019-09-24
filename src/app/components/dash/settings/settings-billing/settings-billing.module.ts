d11b24479a943328cf3b34133b98f621522e4fb5
Arsalan Savand <arsalan@savandbros.com>
2019-09-24 14:49:40 +0300

    Use relative paths

--- src/app/components/dash/settings/settings-billing/settings-billing.module.ts
+++ src/app/components/dash/settings/settings-billing/settings-billing.module.ts
@@ -1,12 +1,12 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
-import { SettingsBillingComponent } from '@app/components/dash/settings/settings-billing/settings-billing.component';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { library } from '@fortawesome/fontawesome-svg-core';
 import { faCircleNotch } from '@fortawesome/free-solid-svg-icons/faCircleNotch';
 import { TranslateModule } from '@ngx-translate/core';
 import { ModalModule } from 'ngx-bootstrap/modal';
-import { SettingsBillingRoutingModule } from 'src/app/components/dash/settings/settings-billing/settings-billing-routing.module';
+import { SettingsBillingRoutingModule } from './settings-billing-routing.module';
+import { SettingsBillingComponent } from './settings-billing.component';
 
 
 @NgModule({
