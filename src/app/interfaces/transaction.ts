00150968c6839fb13e4ff38f60e4730773345d7d
Arsalan Savand <arsalan@savandbros.com>
2019-09-24 14:39:15 +0300

    Add documentation to Transaction interface

--- src/app/interfaces/transaction.ts
+++ src/app/interfaces/transaction.ts
@@ -1,5 +1,8 @@
 /**
- * Represents a single transaction information structure
+ * Represents a single transaction information structure such as: Transaction pay date, transaction ID and amount of it
+ * This is being used at billing page
+ *
+ * @link SettingsBillingComponent
  */
 export interface Transaction {
   created: string;
