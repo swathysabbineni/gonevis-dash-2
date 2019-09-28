4c37fdf7cadb9c1ee4361379ffded1d59241cf40
Amir Savand <amir@savandbros.com>
2019-09-28 12:00:18 +0300

    Fix linting

--- src/app/components/dash/settings/settings-upgrade/payment-validation/payment-validation.module.ts
+++ src/app/components/dash/settings/settings-upgrade/payment-validation/payment-validation.module.ts
@@ -1,20 +1,24 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
-import { PaymentValidationComponent } from '@app/components/dash/settings/settings-upgrade/payment-validation/payment-validation.component';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { library } from '@fortawesome/fontawesome-svg-core';
 import { faCircleNotch } from '@fortawesome/free-solid-svg-icons/faCircleNotch';
 import { TranslateModule } from '@ngx-translate/core';
 
+import { PaymentValidationComponent } from './payment-validation.component';
 
 @NgModule({
-  declarations: [PaymentValidationComponent],
+  declarations: [
+    PaymentValidationComponent,
+  ],
   imports: [
     CommonModule,
     FontAwesomeModule,
     TranslateModule,
   ],
-  entryComponents: [PaymentValidationComponent],
+  entryComponents: [
+    PaymentValidationComponent,
+  ],
 })
 export class PaymentValidationModule {
   constructor() {
--- src/app/components/user/user-setting/user-password/user-password.module.ts
+++ src/app/components/user/user-setting/user-password/user-password.module.ts
@@ -1,14 +1,17 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { UserPasswordRoutingModule } from '@app/components/user/user-setting/user-password/user-password-routing.module';
-import { UserPasswordComponent } from '@app/components/user/user-setting/user-password/user-password.component';
 import { PageCoverModule } from '@app/shared/page-cover/page-cover.module';
 import { PasswordStrengthModule } from '@app/shared/password-strength/password-strength.module';
 import { TranslateModule } from '@ngx-translate/core';
 
+import { UserPasswordRoutingModule } from './user-password-routing.module';
+import { UserPasswordComponent } from './user-password.component';
+
 @NgModule({
-  declarations: [UserPasswordComponent],
+  declarations: [
+    UserPasswordComponent,
+  ],
   imports: [
     CommonModule,
     UserPasswordRoutingModule,
