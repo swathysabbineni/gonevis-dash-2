a257c48933f524144febde4de93acbca8fee0a76
Amir Savand <amir@savandbros.com>
2019-10-06 14:14:44 +0300

    Change from "EntryShare" to "Share"

--- src/app/shared/entry-list/entry-list.module.ts
+++ src/app/shared/entry-list/entry-list.module.ts
@@ -2,7 +2,7 @@ import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { EmbedModule } from '@app/shared/embed/embed.module';
-import { EntryShareModule } from '@app/shared/entry-share/entry-share.module';
+import { ShareModule } from '@app/shared/share/share.module';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { library } from '@fortawesome/fontawesome-svg-core';
 import { faBookmark } from '@fortawesome/free-regular-svg-icons/faBookmark';
@@ -32,7 +32,7 @@ import { EntryListComponent } from './entry-list.component';
     TooltipModule.forRoot(),
     InfiniteScrollModule,
     PopoverModule.forRoot(),
-    EntryShareModule,
+    ShareModule,
     EmbedModule,
   ],
   exports: [
