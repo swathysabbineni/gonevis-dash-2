55e4734d31abf7c13eea4875d60fe23aaf433106
Arsalan Savand <arsalan@savandbros.com>
2019-10-06 15:09:12 +0300

    Update Clipboard module

--- src/app/components/dash/write/modules/clipboard.ts
+++ src/app/components/dash/write/modules/clipboard.ts
@@ -1,26 +1,22 @@
-import Quill, { BoundsStatic, RangeStatic } from 'quill';
+import Quill, { RangeStatic } from 'quill';
 import Delta from 'quill-delta';
 
 const ClipboardModule: any = Quill.import('modules/clipboard');
 
 class CustomClipboard extends ClipboardModule {
-  onPaste(e: ClipboardEvent): void {
-    if (e.defaultPrevented || !this.quill.isEnabled()) {
+  onPaste(event: ClipboardEvent): void {
+    if (event.defaultPrevented || !this.quill.isEnabled()) {
       return;
     }
     const range: RangeStatic = this.quill.getSelection();
     let delta: Delta = new Delta().retain(range.index);
-    this.container.style.top = (window.pageYOffset ||
-      document.documentElement.scrollTop ||
-      document.body.scrollTop || 0).toString() + 'px';
     this.container.focus();
     setTimeout((): void => {
       this.quill.selection.update('silent');
       delta = delta.concat(this.convert()).delete(range.length);
       this.quill.updateContents(delta, 'user');
       this.quill.setSelection(delta.length() - range.length, range.length, 'silent');
-      const bounds: BoundsStatic = this.quill.getBounds(delta.length() - range.length, range.length);
-      this.quill.scrollingContainer.scrollTop = bounds.top;
+      this.quill.scrollIntoView();
     }, 1);
   }
 }
