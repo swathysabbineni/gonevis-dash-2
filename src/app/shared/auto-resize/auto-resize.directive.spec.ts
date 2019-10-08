0eb7ee0a1e58edd9eec4a28c0ae0c38e9036e792
Arsalan Savand <arsalan@savandbros.com>
2019-10-08 15:52:56 +0300

    Remove AutoResizeDirective

--- src/app/shared/auto-resize/auto-resize.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { AutoResizeDirective } from 'src/app/shared/auto-resize/auto-resize.directive';
-
-describe('AutoResizeDirective', () => {
-  it('should create an instance', () => {
-    const directive = new AutoResizeDirective();
-    expect(directive).toBeTruthy();
-  });
-});
