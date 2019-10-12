b77d486609608bc99c3e4a255bf0145540a113bd
Amir Savand <amir@savandbros.com>
2019-10-12 16:53:04 +0300

    Make action and filter work

--- src/app/components/dash/entry/entry.component.ts
+++ src/app/components/dash/entry/entry.component.ts
@@ -1,6 +1,10 @@
+import { HttpErrorResponse } from '@angular/common/http';
+import { isSyntheticPropertyOrListener } from '@angular/compiler/src/render3/util';
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
+import { EntryStatus } from '@app/enums/entry-status.enum';
 import { ApiResponse } from '@app/interfaces/api-response';
+import { Filter } from '@app/interfaces/filter';
 import { Pagination } from '@app/interfaces/pagination';
 import { Entry } from '@app/interfaces/v1/entry';
 import { BlogMin } from '@app/interfaces/zero/user/blog-min';
@@ -22,10 +26,29 @@ export class EntryComponent implements OnInit {
   readonly filter: IconDefinition = faFilter;
 
   /**
+   * Exposed for view
+   */
+  readonly entryStatus = EntryStatus;
+
+  /**
    * Is showing posts or pages
    * @see DashRoutingModule
    */
   readonly isPages: boolean = this.route.snapshot.data.pages === true;
+
+  /**
+   * List of status filters
+   */
+  readonly statusFilters: Filter<EntryStatus>[] = [{
+    value: '',
+    label: 'ANY_STATUS',
+  }, {
+    value: EntryStatus.Published,
+    label: 'PUBLISHED',
+  }, {
+    value: EntryStatus.Draft,
+    label: 'DRAFT',
+  }];
 
   /**
    * List of entries (pages or posts)
@@ -43,22 +66,27 @@ export class EntryComponent implements OnInit {
   loading: boolean;
 
   /**
+   * Current status filter
+   */
+  statusFilter: Filter<EntryStatus> = this.statusFilters[1];
+
+  /**
    * Main select checkbox (used to select and deselect all)
    */
   select: boolean;
 
   /**
-   * @returns Number of selected entries
+   * @returns Selected entries
    */
-  get selectedCount(): number {
-    return this.entries.filter(entry => entry.select).length;
+  get entriesSelected(): Entry[] {
+    return this.entries.filter(entry => entry.select);
   }
 
   /**
    * @returns Whether select is indeterminate or not
    */
   get isSelectIndeterminate(): boolean {
-    return Boolean(this.entries.length > this.selectedCount && this.selectedCount > 0);
+    return Boolean(this.entries.length > this.entriesSelected.length && this.entriesSelected.length > 0);
   }
 
   constructor(private entryService: EntryService,
@@ -82,10 +110,17 @@ export class EntryComponent implements OnInit {
   /**
    * On main select checked or unchecked
    */
-  onToggleSelect() {
+  onToggleSelect(): void {
     for (const entry of this.entries) {
       entry.select = this.select;
     }
+  }
+
+  /**
+   * On entry selection changed
+   */
+  onEntrySelect(): void {
+    this.select = this.entriesSelected.length === this.entries.length;
   }
 
   /**
@@ -93,10 +128,11 @@ export class EntryComponent implements OnInit {
    *
    * @param page Page number
    */
-  getEntries(page: number = 1) {
+  getEntries(page: number = 1): void {
     this.loading = true;
     this.entryService.getEntries({
       is_page: this.isPages,
+      status: this.statusFilter.value,
     }, page).subscribe((response: ApiResponse<Entry>): void => {
       this.pagination = {
         itemsPerPage: EntryService.PAGE_SIZE,
@@ -104,6 +140,7 @@ export class EntryComponent implements OnInit {
       };
       this.entries = response.results;
       this.loading = false;
+      this.entries[0].created = new Date(this.entries[0].created).toUTCString();
     });
   }
 
@@ -146,5 +183,24 @@ export class EntryComponent implements OnInit {
    */
   pageChanged(event: PageChangedEvent) {
     this.getEntries(event.page);
+  }
+
+  /**
+   * Bulk action to update selected entries
+   *
+   * @param property Updating property name
+   * @param value Updating property value
+   */
+  updateEntries(property: keyof Entry, value: any): void {
+    for (const entry of this.entriesSelected) {
+      const PAYLOAD: { [property: string]: any } = {};
+      PAYLOAD[property] = value;
+      this.entryService.update(entry.id, PAYLOAD).subscribe(((data: Entry): void => {
+        entry[property] = data[property];
+        this.toast.info(this.translate.instant('TOAST_UPDATE'), entry.title);
+      }), (error: HttpErrorResponse): void => {
+        console.error('Failed to update selected entry', entry.id, property, value, error.error);
+      });
+    }
   }
 }
