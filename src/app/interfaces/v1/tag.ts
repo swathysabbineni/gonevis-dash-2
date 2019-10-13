91fc9e9fc01237d44ffebe4d387729a93c2dc9c4
Amer Ansari <amer@savandbros.com>
2019-10-13 19:13:22 +0400

    Update tag.ts

--- src/app/interfaces/v1/tag.ts
+++ src/app/interfaces/v1/tag.ts
@@ -1,3 +1,5 @@
+import { File } from '@app/interfaces/file';
+
 export interface Tag {
   id: string;
   name: string;
@@ -6,24 +8,7 @@ export interface Tag {
   meta_description?: any;
   site: string;
   media: {
-    cover_image: {
-      id: string;
-      file: string;
-      thumbnail_256x256: string;
-      thumbnail_128x128: string;
-      thumbnail_48x48: string;
-      ext: string;
-      meta_data: {
-        description?: any;
-        name: string;
-      };
-      user: string;
-      file_name: string;
-      size_human: string;
-      is_image: boolean;
-      created: string;
-      updated: string;
-    }
+    cover_image: File;
   };
   tagged_items_count: number;
   absolute_uri: string;
