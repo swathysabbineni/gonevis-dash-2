c79613a211b09b8985a26c91d9d3de557d537f56
Amer Ansari <amer@savandbros.com>
2019-10-13 19:19:10 +0400

    Add cover image for edition tag

--- src/app/shared/tags-modal/tag-modal.component.ts
+++ src/app/shared/tags-modal/tag-modal.component.ts
@@ -1,9 +1,10 @@
-import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Component, OnInit, TemplateRef } from '@angular/core';
+import { FormBuilder, FormGroup, Validators, FormControl } from '@angular/forms';
 import { ApiError } from '@app/interfaces/api-error';
+import { File } from '@app/interfaces/file';
 import { Tag } from '@app/interfaces/v1/tag';
 import { BlogService } from '@app/services/blog/blog.service';
-import { BsModalRef } from 'ngx-bootstrap';
+import { BsModalRef, BsModalService } from 'ngx-bootstrap';
 
 @Component({
   selector: 'app-tag-modal',
@@ -21,6 +22,11 @@ export class TagModalComponent implements OnInit {
    * Tag form
    */
   form: FormGroup;
+
+  /**
+   * Tag form image
+   */
+  image: File;
 
   /**
    * Tag form API errors
@@ -46,6 +52,10 @@ export class TagModalComponent implements OnInit {
       slug: [this.tag.slug],
       description: [this.tag.description],
     });
+    /**
+     * Show current tag image if there are
+     */
+    this.image = this.tag.media.cover_image;
   }
 
   /**
@@ -53,6 +63,9 @@ export class TagModalComponent implements OnInit {
    */
   update(): void {
     this.loading = true;
+    if (this.image) {
+      this.form.addControl('cover_image', new FormControl(this.image.id));
+    }
     this.blogService.updateTag(this.tag.slug, this.form.value).subscribe((data: Tag): void => {
       this.loading = false;
       this.errors = {};
