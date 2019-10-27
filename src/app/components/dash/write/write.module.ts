e2c1d77f691025c9a7094fa3d10c0d0d3c46cde6
Arsalan Savand <arsalan@savandbros.com>
2019-10-27 13:50:58 +0300

    Update WriteModule icons

--- src/app/components/dash/write/write.module.ts
+++ src/app/components/dash/write/write.module.ts
@@ -9,16 +9,17 @@ import { faNewspaper } from '@fortawesome/free-regular-svg-icons/faNewspaper';
 import { faAngleDown } from '@fortawesome/free-solid-svg-icons/faAngleDown';
 import { faCheck } from '@fortawesome/free-solid-svg-icons/faCheck';
 import { faCog } from '@fortawesome/free-solid-svg-icons/faCog';
+import { faEye } from '@fortawesome/free-solid-svg-icons/faEye';
+import { faHashtag } from '@fortawesome/free-solid-svg-icons/faHashtag';
 import { faImage } from '@fortawesome/free-solid-svg-icons/faImage';
 import { faPlus } from '@fortawesome/free-solid-svg-icons/faPlus';
-import { faTags } from '@fortawesome/free-solid-svg-icons/faTags';
 import { faTimes } from '@fortawesome/free-solid-svg-icons/faTimes';
 import { faUndo } from '@fortawesome/free-solid-svg-icons/faUndo';
 import { TranslateModule } from '@ngx-translate/core';
 import { AutosizeModule } from 'ngx-autosize';
+import { CollapseModule } from 'ngx-bootstrap/collapse';
 import { BsDatepickerModule } from 'ngx-bootstrap/datepicker';
 import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
-import { CollapseModule } from 'ngx-bootstrap/collapse';
 import { ModalModule } from 'ngx-bootstrap/modal';
 import { PopoverModule } from 'ngx-bootstrap/popover';
 import { TooltipModule } from 'ngx-bootstrap/tooltip';
@@ -53,11 +54,12 @@ import { WriteComponent } from './write.component';
 })
 export class WriteModule {
   constructor() {
+    library.add(faEye);
     library.add(faCog);
     library.add(faTimes);
     library.add(faNewspaper);
     library.add(faAngleDown);
-    library.add(faTags);
+    library.add(faHashtag);
     library.add(faImage);
     library.add(faPlus);
     library.add(faUndo);
