6b366a2310b09c4f84133d0ed71636bf1525b608
Amer Ansari <amer@savandbros.com>
2019-11-02 20:41:56 +0400

    Decleare write icons

--- src/app/components/dash/write/write.module.ts
+++ src/app/components/dash/write/write.module.ts
@@ -4,17 +4,6 @@ import { NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { FileListModule } from '@app/shared/file-list/file-list.module';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
-import { library } from '@fortawesome/fontawesome-svg-core';
-import { faNewspaper } from '@fortawesome/free-regular-svg-icons/faNewspaper';
-import { faAngleDown } from '@fortawesome/free-solid-svg-icons/faAngleDown';
-import { faCheck } from '@fortawesome/free-solid-svg-icons/faCheck';
-import { faCog } from '@fortawesome/free-solid-svg-icons/faCog';
-import { faEye } from '@fortawesome/free-solid-svg-icons/faEye';
-import { faHashtag } from '@fortawesome/free-solid-svg-icons/faHashtag';
-import { faImage } from '@fortawesome/free-solid-svg-icons/faImage';
-import { faPlus } from '@fortawesome/free-solid-svg-icons/faPlus';
-import { faTimes } from '@fortawesome/free-solid-svg-icons/faTimes';
-import { faUndo } from '@fortawesome/free-solid-svg-icons/faUndo';
 import { TranslateModule } from '@ngx-translate/core';
 import { AutosizeModule } from 'ngx-autosize';
 import { CollapseModule } from 'ngx-bootstrap/collapse';
@@ -53,16 +42,4 @@ import { WriteComponent } from './write.component';
   ],
 })
 export class WriteModule {
-  constructor() {
-    library.add(faEye);
-    library.add(faCog);
-    library.add(faTimes);
-    library.add(faNewspaper);
-    library.add(faAngleDown);
-    library.add(faHashtag);
-    library.add(faImage);
-    library.add(faPlus);
-    library.add(faUndo);
-    library.add(faCheck);
-  }
 }
