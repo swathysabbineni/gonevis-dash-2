87cf1a779f381a33a923d4683a4ca20cb01fc70c
Amer Ansari <amer@savandbros.com>
2019-11-02 18:12:46 +0400

    Decleare navs icons

--- src/app/components/dash/navs/navs.module.ts
+++ src/app/components/dash/navs/navs.module.ts
@@ -4,10 +4,6 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { LoadingModule } from '@app/shared/loading/loading.module';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
-import { library } from '@fortawesome/fontawesome-svg-core';
-import { faGripLinesVertical } from '@fortawesome/free-solid-svg-icons/faGripLinesVertical';
-import { faPlus } from '@fortawesome/free-solid-svg-icons/faPlus';
-import { faTrash } from '@fortawesome/free-solid-svg-icons/faTrash';
 import { TranslateModule } from '@ngx-translate/core';
 
 import { NavsRoutingModule } from './navs-routing.module';
@@ -29,9 +25,4 @@ import { NavsComponent } from './navs.component';
   ],
 })
 export class NavsModule {
-  constructor() {
-    library.add(faGripLinesVertical);
-    library.add(faPlus);
-    library.add(faTrash);
-  }
 }
