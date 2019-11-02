faf156bcca6495faa623fe7f0d59b5fd36616f63
Amer Ansari <amer@savandbros.com>
2019-11-02 05:29:12 +0400

    Decleare help icons

--- src/app/components/dash/help/help.module.ts
+++ src/app/components/dash/help/help.module.ts
@@ -1,9 +1,6 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
-import { library } from '@fortawesome/fontawesome-svg-core';
-import { faDiscord } from '@fortawesome/free-brands-svg-icons/faDiscord';
-import { faCheckCircle } from '@fortawesome/free-regular-svg-icons/faCheckCircle';
 import { TranslateModule } from '@ngx-translate/core';
 
 import { HelpRoutingModule } from './help-routing.module';
@@ -22,8 +19,4 @@ import { HelpComponent } from './help.component';
   ],
 })
 export class HelpModule {
-  constructor() {
-    library.add(faCheckCircle);
-    library.add(faDiscord);
-  }
 }
