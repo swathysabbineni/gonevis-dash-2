480742cad1132ff8be3f6b30e6eb76475db42ca4
Amer Ansari <amer@savandbros.com>
2019-11-02 19:21:50 +0400

    Decleare settings-upgrade icons

--- src/app/components/dash/settings/settings-upgrade/settings-upgrade.module.ts
+++ src/app/components/dash/settings/settings-upgrade/settings-upgrade.module.ts
@@ -2,10 +2,6 @@ import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { SettingsUpgradeComponent } from '@app/components/dash/settings/settings-upgrade/settings-upgrade.component';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
-import { library } from '@fortawesome/fontawesome-svg-core';
-import { faCheck } from '@fortawesome/free-solid-svg-icons/faCheck';
-import { faDollarSign } from '@fortawesome/free-solid-svg-icons/faDollarSign';
-import { faStar } from '@fortawesome/free-solid-svg-icons/faStar';
 import { TranslateModule } from '@ngx-translate/core';
 import { ModalModule } from 'ngx-bootstrap/modal';
 import { PaymentValidationModule } from './payment-validation/payment-validation.module';
@@ -25,9 +21,4 @@ import { SettingsUpgradeRoutingModule } from './settings-upgrade-routing.module'
   ],
 })
 export class SettingsUpgradeModule {
-  constructor() {
-    library.add(faStar);
-    library.add(faDollarSign);
-    library.add(faCheck);
-  }
 }
