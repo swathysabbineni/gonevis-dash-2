2f05bb94c6c30e2f39dc4a6c2f2eabe4b0378962
Amer Ansari <amer@savandbros.com>
2019-11-04 03:04:34 +0400

    Decleare feed/entry icons

--- src/app/components/feed/entry/entry.module.ts
+++ src/app/components/feed/entry/entry.module.ts
@@ -6,16 +6,6 @@ import { CommentFormModule } from '@app/shared/comment-form/comment-form.module'
 import { EntryListModule } from '@app/shared/entry-list/entry-list.module';
 import { ShareModule } from '@app/shared/share/share.module';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
-import { library } from '@fortawesome/fontawesome-svg-core';
-import { faBookmark } from '@fortawesome/free-regular-svg-icons/faBookmark';
-import { faComment } from '@fortawesome/free-regular-svg-icons/faComment';
-import { faEye } from '@fortawesome/free-regular-svg-icons/faEye';
-import { faHeart } from '@fortawesome/free-regular-svg-icons/faHeart';
-import { faShareSquare } from '@fortawesome/free-regular-svg-icons/faShareSquare';
-import { faHeart as faHeartFill, faPaperPlane } from '@fortawesome/free-solid-svg-icons';
-import { faBookmark as faBookmarkFill } from '@fortawesome/free-solid-svg-icons/faBookmark';
-import { faEdit } from '@fortawesome/free-solid-svg-icons/faEdit';
-import { faTrash } from '@fortawesome/free-solid-svg-icons/faTrash';
 import { TranslateModule } from '@ngx-translate/core';
 import { PopoverModule, TooltipModule } from 'ngx-bootstrap';
 import { InfiniteScrollModule } from 'ngx-infinite-scroll';
@@ -40,16 +30,4 @@ import { EntryRoutingModule } from './entry-routing.module';
   ],
 })
 export class EntryModule {
-  constructor() {
-    library.add(faHeart);
-    library.add(faHeartFill);
-    library.add(faComment);
-    library.add(faEye);
-    library.add(faBookmark);
-    library.add(faBookmarkFill);
-    library.add(faShareSquare);
-    library.add(faPaperPlane);
-    library.add(faEdit);
-    library.add(faTrash);
-  }
 }
