1f24d1d83a98ce1a95df9d5fbd148d9f8fe84045
Amir Savand <amir@savandbros.com>
2020-02-21 01:10:29 +0300

    Handle all errors (WIP)

--- src/app/services/auth-interceptor/auth-interceptor.service.ts
+++ src/app/services/auth-interceptor/auth-interceptor.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpErrorResponse, HttpEvent, HttpHandler, HttpInterceptor, HttpRequest } from '@angular/common/http';
+import { ApiError } from '@app/interfaces/api-error';
 import { HttpErrorResponseApi } from '@app/models/http-error-response-api';
 import { environment } from '@environments/environment';
 import { CookieService } from 'ngx-cookie-service';
@@ -14,17 +15,17 @@ import { AuthService } from '../auth/auth.service';
 export class AuthInterceptorService implements HttpInterceptor {
 
   constructor(private cookieService: CookieService,
-              private authService: AuthService,
-              private toast: ToastrService) {
+              private authService: AuthService) {
   }
 
   /**
-   * @param request The outgoing request to handle
-   * @param next The next interceptor in the chain, or the backend if no interceptors in the chain.
+   * Modify headers and error handling
    */
   intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-    const token: string = this.authService.getToken();
-    // Add authorization header with bearer token if available.
+    /**
+     * Add authorization to headers
+     */
+    const token = this.authService.getToken();
     if (token && request.url.includes(environment.api.v1) || request.url.includes(environment.api.zero)) {
       request = request.clone({
         setHeaders: {
@@ -32,35 +33,54 @@ export class AuthInterceptorService implements HttpInterceptor {
         },
       });
     }
-    return next.handle(request).pipe(catchError((error: HttpErrorResponse): Observable<never> => {
-      if (error.error instanceof ErrorEvent) {
-        // A client-side or network error occurred. Handle it accordingly.
-        console.error('An error occurred:', error.error.message);
-      } else {
-        // Sign out if 403 response
-        if (error.status === 403) {
-          if (error.error.detail.indexOf('You do not have permission to perform this action.') !== -1) {
-            this.authService.signOut();
+    /**
+     * Error handling
+     */
+    return next.handle(request).pipe(catchError((httpErrorResponse: HttpErrorResponse): Observable<never> => {
+      /**
+       * Convert error response
+       */
+      const error: HttpErrorResponseApi = new HttpErrorResponseApi(httpErrorResponse);
+      /**
+       * Handle error status cases
+       */
+      switch (error.status) {
+        case 400: {
+          /**
+           * User email is not verified
+           */
+          if (error.error.non_field_errors.includes('Your email is not verified.')) {
+            // @todo
           }
-          if (error.error.detail.indexOf('You need to upgrade your subscription plan to make such action.') !== -1) {
-            /**
-             * Find duplicated toast based on message
-             */
-            const duplicatedToast: ActiveToast<any> = this.toast.findDuplicate(error.error.detail, false, true);
-            /**
-             * On duplicated toast found, clear the it before showing the message again.
-             */
-            if (duplicatedToast) {
-              this.toast.remove(duplicatedToast.toastId);
-            }
-            this.toast.error(error.error.detail);
-          }
+          break;
         }
-        // The backend returned an unsuccessful response code.
-        // The response body may contain clues as to what went wrong,
-        console.error('Backend returned code:', error.status, 'body was: ', error.error);
+        case 401: {
+          /**
+           * User authentication token is invalid
+           */
+          // @todo
+          break;
+        }
+        case 403: {
+          /**
+           * User does not have permission for this action
+           */
+          if (error.error.detail === 'You do not have permission to perform this action.') {
+            // @todo
+          }
+          /**
+           * User blog needs to be upgrade for this action
+           */
+          if (error.error.detail === 'You need to upgrade your subscription plan to make such action.') {
+            // @todo
+          }
+          break;
+        }
       }
-      return throwError(new HttpErrorResponseApi(error));
+      /**
+       * Return the error
+       */
+      return throwError(error);
     }));
   }
 }
