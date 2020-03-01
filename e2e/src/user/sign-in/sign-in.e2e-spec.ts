23000b1d596cac4d0cc14ad221e65b99a6f3cd6e
Amir Savand <amir@savandbros.com>
2020-03-01 11:15:23 +0300

    E2E test signing in via email too

--- e2e/src/user/sign-in/sign-in.e2e-spec.ts
+++ e2e/src/user/sign-in/sign-in.e2e-spec.ts
@@ -9,20 +9,26 @@ describe('User SignIn', () => {
     page = new SignInPage();
   });
 
-  it('should navigate to it', () => {
+  it('should sign in via username', () => {
     page.navigateTo();
+    page.fillForm(SignInPage.DATA.username, SignInPage.DATA.password);
+    browser.wait(ExpectedConditions.urlContains('/dash/0/main'), 5000);
   });
 
-  it('should sign in', () => {
-    page.navigateTo();
-    page.fillForm();
-    browser.wait(ExpectedConditions.urlContains('/dash/0/main'), 5000);
+  it('should sign in via email', async () => {
+    // Wait for username dropdown to show up
+    browser.wait(ExpectedConditions.visibilityOf(page.getUserDropdown()), 5000).then(() => {
+      // Sign out and user will be redirected to sign-in page again
+      page.signOut();
+      // Login in again but with email instead of username
+      page.fillForm(SignInPage.DATA.email, SignInPage.DATA.password);
+      // Should redirect to dash
+      browser.wait(ExpectedConditions.urlContains('/dash/0/main'), 5000);
+    });
   });
 
   afterEach(async () => {
     const logs = await browser.manage().logs().get(logging.Type.BROWSER);
-    expect(logs).not.toContain(jasmine.objectContaining({
-      level: logging.Level.SEVERE,
-    } as logging.Entry));
+    expect(logs).not.toContain(jasmine.objectContaining({ level: logging.Level.SEVERE } as logging.Entry));
   });
 });
--- e2e/src/user/sign-in/sign-in.po.ts
+++ e2e/src/user/sign-in/sign-in.po.ts
@@ -1,20 +1,29 @@
-import { browser, element, by } from 'protractor';
+import { browser, element, by, ElementFinder } from 'protractor';
 
 export class SignInPage {
 
-  private static readonly CREDENTIALS = {
+  static readonly DATA = {
     email: 'e2e@gonevis.com',
     username: 'e2e',
     password: 'password',
   };
 
-  navigateTo() {
-    return browser.get('/user/sign-in');
+  navigateTo(): void {
+    browser.get('/user/sign-in');
   }
 
-  fillForm(credentials = SignInPage.CREDENTIALS) {
-    element(by.css('[formcontrolname=username]')).sendKeys(credentials.username);
-    element(by.css('[formcontrolname=password]')).sendKeys(credentials.password);
+  fillForm(username: string, password: string): void {
+    element(by.css('[formcontrolname=username]')).sendKeys(username);
+    element(by.css('[formcontrolname=password]')).sendKeys(password);
     element(by.css('[type=submit]')).click();
+  }
+
+  getUserDropdown(): ElementFinder {
+    return element(by.cssContainingText('span', SignInPage.DATA.username));
+  }
+
+  signOut(): void {
+    this.getUserDropdown().click();
+    element(by.cssContainingText('button', 'Sign Out')).click();
   }
 }
