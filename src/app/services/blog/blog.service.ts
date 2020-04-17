c0054190418b9d26910bb2c70a76b411e68d59a7
Arsalan Savand <ArsalanSavand@users.noreply.github.com>
2020-04-17 20:37:17 +0300

    Fix payload

--- src/app/services/blog/blog.service.ts
+++ src/app/services/blog/blog.service.ts
@@ -256,9 +256,7 @@ export class BlogService {
    */
   updateFont(payload: Params): Observable<BlogSettings> {
     return this.http.put<BlogSettings>(
-      `${this.api.base.v1}site/${BlogService.currentBlog.id}/set-font/`, {
-        remove_branding: status,
-      },
+      `${this.api.base.v1}site/${BlogService.currentBlog.id}/set-font/`, payload
     );
   }
 
