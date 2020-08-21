4651640cff1913b2d474be19097b5e6735b00f4d
Arsalan Savand <arsalan@savandbros.com>
2020-08-21 11:30:25 +0300

    Update UserAvatarComponent to have dynamic resolution and text color based on background color

--- src/app/shared/user-avatar/user-avatar.component.ts
+++ src/app/shared/user-avatar/user-avatar.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, OnChanges, SimpleChanges } from '@angular/core';
 import { Media } from '@app/interfaces/media';
 
 @Component({
@@ -6,7 +6,7 @@ import { Media } from '@app/interfaces/media';
   templateUrl: './user-avatar.component.html',
   styleUrls: ['./user-avatar.component.scss'],
 })
-export class UserAvatarComponent implements OnInit {
+export class UserAvatarComponent implements OnInit, OnChanges {
 
   /**
    * User's name to generate color from it.
@@ -29,6 +29,11 @@ export class UserAvatarComponent implements OnInit {
   @Input() size = 48;
 
   /**
+   * Resolution of the media based on {@link size}.
+   */
+  resolution: string;
+
+  /**
    * Acronym of given name.
    */
   acronym: string;
@@ -36,15 +41,28 @@ export class UserAvatarComponent implements OnInit {
   /**
    * Color generated from {@link name}.
    */
+  backgroundColor: string;
+
+  /**
+   * Contrast color based on {@link backgroundColor}.
+   */
   color: string;
 
   constructor() {
   }
 
   ngOnInit(): void {
-    this.color = this.stringToColor(this.name);
     this.classList = ['d-flex', 'align-items-center', 'justify-content-center'].concat(this.classList);
-    this.acronym = this.name.match(/\b(\w)/g).join('');
+    /**
+     * Change {@link media} resolution based on given {@link size}.
+     */
+    if (this.size <= 48) {
+      this.resolution = 'thumbnail_48x48';
+    } else if (this.size > 48 && this.size <= 128) {
+      this.resolution = 'thumbnail_128x128';
+    } else if (this.size > 128 && this.size <= 256) {
+      this.resolution = 'thumbnail_256x256';
+    }
   }
 
   /**
@@ -60,5 +78,24 @@ export class UserAvatarComponent implements OnInit {
     const color: string = (hash & 0x00FFFFFF).toString(16).toUpperCase();
 
     return `#${'00000'.substring(0, 6 - color.length)}${color}`;
+  }
+
+  /**
+   * Get the contrast color from given HEX color.
+   *
+   * @param hexColor A hex color value.
+   */
+  getContrast(hexColor: string): string {
+    const red: number = parseInt(hexColor.substr(0, 2), 16);
+    const green: number = parseInt(hexColor.substr(2, 2), 16);
+    const blue: number = parseInt(hexColor.substr(4, 2), 16);
+    const yiq: number = ((red * 299) + (green * 587) + (blue * 114)) / 1000;
+    return (yiq >= 128) ? 'black' : 'white';
+  }
+
+  ngOnChanges(changes: SimpleChanges): void {
+    this.backgroundColor = this.stringToColor(this.name);
+    this.color = this.getContrast(this.backgroundColor);
+    this.acronym = this.name.match(/\b(\w)/g).join('');
   }
 }
